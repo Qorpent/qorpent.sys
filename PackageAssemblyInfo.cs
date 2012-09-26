200a7c0528907cd6fb9db3e4e7875e84a712f261
Fagim Sadykov <fagim.sadykov@gmail.com>
2012-09-26 20:50:13 +0600

    gitall bat file added

---
+++ PackageAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Media Technology Ltd")]
+[assembly: AssemblyProduct("Qorpent")]
+[assembly: AssemblyCopyright("Copyright © 2006 - 2012")]
+[assembly: AssemblyTrademark("Qorpent TM")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
---
+++ gitall.bat
@@ -0,0 +1,24 @@
+for /D %%1 in (*) do (
+	cd %%1
+	git fetch
+	cd ..
+)
+
+
+for /D %%1 in (*) do (
+	cd %%1
+	git add --all
+	git commit -a
+	cd ..
+)
+
+if '%1' == 'push' (
+
+for /D %%1 in (*) do (
+	cd %%1
+	git push
+	cd ..
+)
+
+
+)
