42c61ca8398af9fde468340a79f73b3549b21db8
unknown <sfo@ugmk-as2.ugmk.com>
2012-09-27 14:02:16 +0600

    buildall script added

--- win-init.bat
+++ win-init.bat
@@ -1,4 +1,10 @@
 set sys=%~p0
 set root=%sys%..\
+if "%~n0" == "init" (
+	set sys=.\qorpent.sys\
+	set root=%sys%\
+)
+mklink %root%init.bat %sys%win-init.bat
 mklink %root%syncall.bat %sys%sys-syncall.bat
 mklink %root%installapp %sys%installapp
+mklink %root%buildall %sys%buildall
