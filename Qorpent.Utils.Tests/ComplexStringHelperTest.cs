242d25bde7df6b0e343961f538f450ab59598d1e
Fagim Sadykov <fagim.sadykov@gmail.com>
2012-12-11 13:44:04 +0600

    complex string helper fixes

---
+++ Qorpent.Utils.Tests/ComplexStringHelperTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Qorpent.Utils.Tests
+{
+	[TestFixture]
+	public class ComplexStringHelperTest
+	{
+		[TestCase("/`a:1~~`z/ ", "/a", "1::/z")]
+		[TestCase("/a:1/ /b/ /c:/ /dx:xxx/","a","1","b","","c","","dx","xxx")]
+		public void TagParserTest(string s, params string[] tests) {
+			var dict = ComplexStringHelper.CreateTagParser().Parse(s);
+			for(int i=0;i<tests.Count()-1;i=i+2) {
+				var n = tests[i];
+				var v = tests[i + 1];
+				Assert.AreEqual(dict[n],v);
+			}
+			
+		}
+
+
+		[TestCase("`a:1~~`z|b", "|a", "1::|z","b","")]
+		[TestCase("a:1|b|c|dx:xxx", "a", "1", "b", "", "c", "", "dx", "xxx")]
+		public void CsParserTest(string s, params string[] tests)
+		{
+			var dict = ComplexStringHelper.CreateComplexStringParser().Parse(s);
+			for (int i = 0; i < tests.Count() - 1; i = i + 2)
+			{
+				var n = tests[i];
+				var v = tests[i + 1];
+				Assert.AreEqual(dict[n], v);
+			}
+		}
+
+		[TestCase("`a:1~~`z b", " a", "1:: z", "b", "")]
+		[TestCase("a:1 b c dx:xxx", "a", "1", "b", "", "c", "", "dx", "xxx")]
+		public void WsCsParserTest(string s, params string[] tests)
+		{
+			var dict = ComplexStringHelper.CreateWSComplexStringParser().Parse(s);
+			for (int i = 0; i < tests.Count() - 1; i = i + 2)
+			{
+				var n = tests[i];
+				var v = tests[i + 1];
+				Assert.AreEqual(dict[n], v);
+			}
+		}
+
+
+		[TestCase("`a:1~~`z b", " a", "1:: z", "b", "")]
+		[TestCase("a:1 b c dx:xxx", "a", "1", "b", "", "c", "", "dx", "xxx")]
+		[TestCase("`a:1~~`z|b", "|a", "1::|z")]
+		[TestCase("a:1|b|c|dx:xxx", "a", "1", "b", "", "c", "", "dx", "xxx")]
+		[TestCase("/`a:1~~`z/ ", "/a", "1::/z")]
+		[TestCase("/a:1/ /b/ /c:/ /dx:xxx/", "a", "1", "b", "", "c", "", "dx", "xxx")]
+		public void AutoDetectParserTest(string s, params string[] tests)
+		{
+			var dict = ComplexStringHelper.AutoDetect(s).Parse(s);
+			for (int i = 0; i < tests.Count() - 1; i = i + 2)
+			{
+				var n = tests[i];
+				var v = tests[i + 1];
+				Assert.AreEqual(dict[n], v);
+			}
+
+		}
+	}
+}
--- Qorpent.Utils/ComplexStringHelper.cs
+++ Qorpent.Utils/ComplexStringHelper.cs
@@ -1,55 +1,267 @@
+#region LICENSE
+
+// Copyright 2007-2012 Comdiv (F. Sadykov) - http://code.google.com/u/fagim.sadykov/
+// Supported by Media Technology LTD 
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//  
+// http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// Solution: Qorpent
+// Original file : ComplexStringHelper.cs
+// Project: Qorpent.Utils
+// 
+// ALL MODIFICATIONS MADE TO FILE MUST BE DOCUMENTED IN SVN
+
+#endregion
+
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Qorpent.Utils.Extensions;
+
 namespace Qorpent.Utils {
 	/// <summary>
-	/// Universal parser for lists and dictionaries
+	/// 	Universal parser for lists and dictionaries
 	/// </summary>
-	public class ComplexStringHelper {
+	public sealed class ComplexStringHelper {
+
+
+		private const string TagLikePrototype = @"{0}(?<name>{4})({1}(?<value>[\s\S]*?))?{2}";
+		private const string CsLikePrototype = @"(^|{3})(?<name>{4})({1}(?<value>[\s\S]+?))?({3}|$)";
+
+		private Regex ParseRegex {
+			get {
+				if (null == _parseregex) {
+					string regexstr = ItemDelimiter.IsNotEmpty() ? CsLikePrototype : TagLikePrototype;
+					regexstr = string.Format(regexstr, ItemPrefix, ValueDelimiter, ItemSuffix, ItemDelimiter, @"[\s\S]+?");
+					_parseregex = new Regex(regexstr, RegexOptions.Compiled);
+				}
+				return _parseregex;
+			}
+		}
+
 		/// <summary>
-		/// Префикс элемента строки
+		/// 	Префикс элемента строки
 		/// </summary>
 		public string ItemPrefix { get; set; }
+
 		/// <summary>
-		/// Суффкс элемента
+		/// 	Суффкс элемента
 		/// </summary>
 		public string ItemSuffix { get; set; }
+
 		/// <summary>
-		/// Разделитель элементов
+		/// 	Разделитель элементов
 		/// </summary>
 		public string ItemDelimiter { get; set; }
+
 		/// <summary>
-		/// Разделитель имя-значение
+		/// 	Разделитель имя-значение
 		/// </summary>
 		public string ValueDelimiter { get; set; }
+
 		/// <summary>
-		/// Значение при отсутствии элемента
+		/// 	Значение при отсутствии элемента
 		/// </summary>
 		public string NotExistedValue { get; set; }
+
 		/// <summary>
-		/// Значение при пустом элементе
+		/// 	Значение при пустом элементе
 		/// </summary>
 		public string EmptyValue { get; set; }
+
 		/// <summary>
-		/// Строка - экран для  разделителя
+		/// 	Строка - экран для  разделителя
 		/// </summary>
 		public string ItemDelimiterSubstitution { get; set; }
+
 		/// <summary>
-		/// Строка - экран для префикса разделителя
+		/// 	Строка - экран для префикса разделителя
 		/// </summary>
 		public string ItemPrefixSubstitution { get; set; }
+
 		/// <summary>
-		/// Строка - экран для суффикса разделителя
+		/// 	Строка - экран для суффикса разделителя
 		/// </summary>
 		public string ItemSuffixSubstitution { get; set; }
+
 		/// <summary>
-		/// Строка - экран разделителя имя-значение
+		/// 	Строка - экран разделителя имя-значение
 		/// </summary>
 		public string ValueDelimiterSubstitution { get; set; }
 
+		
+
+		private string Escape(string str) {
+			if (str.IsEmpty()) {
+				return "";
+			}
+			var preformatedString = str;
+			if(!string.IsNullOrEmpty(ItemDelimiter)) {
+				preformatedString = preformatedString.Replace(ItemDelimiter, ItemDelimiterSubstitution);
+			}
+			if (!string.IsNullOrEmpty(ItemPrefix))
+			{
+				preformatedString = preformatedString.Replace(ItemPrefix, ItemPrefixSubstitution);
+			}
+			if (!string.IsNullOrEmpty(ItemSuffix))
+			{
+				preformatedString = preformatedString.Replace(ItemSuffix, ItemSuffixSubstitution);
+			}
+			if (!string.IsNullOrEmpty(ValueDelimiter))
+			{
+				preformatedString = preformatedString.Replace(ValueDelimiter, ValueDelimiterSubstitution);
+			}
+			return preformatedString;
+		}
+
+		private string UnEscape(string str) {
+			if (str.IsEmpty()) {
+				return "";
+			}
+			var preformatedString = str;
+			if(ItemDelimiterSubstitution.IsNotEmpty()) {
+				preformatedString = preformatedString.Replace(ItemDelimiterSubstitution, ItemDelimiter);
+			}
+			if(ItemPrefixSubstitution.IsNotEmpty()) {
+				preformatedString = preformatedString.Replace(ItemPrefixSubstitution, ItemPrefix);
+			}
+			if(ItemSuffixSubstitution.IsNotEmpty()) {
+				preformatedString = preformatedString.Replace(ItemSuffixSubstitution, ItemSuffix);
+			}
+			if(ValueDelimiterSubstitution.IsNotEmpty()) {
+				preformatedString = preformatedString.Replace(ValueDelimiterSubstitution, ValueDelimiter);
+			}
+			return preformatedString;
+		}
 
 		/// <summary>
-		/// Создает парсер строк вида /x:val/ /y/ /z:~\~/
+		/// 	Парсит входную строку как комплексную
 		/// </summary>
-		/// <returns></returns>
-		public static ComplexStringHelper CreateTagParser () {
+		/// <param name="complexstring"> </param>
+		/// <returns> </returns>
+		public IDictionary<string, string> Parse(string complexstring) {
+			var result = new Dictionary<string, string>();
+			if (complexstring.IsEmpty()) {
+				return result;
+			}
+			if(OptimizedParser || !string.IsNullOrEmpty(ItemDelimiter)) {
+				ParseCS(complexstring, result);
+			}
+			else {
+				ParseTags(complexstring, result);
+			}
+			return result;
+		}
+
+		private void ParseTags(string complexstring, Dictionary<string, string> result) {
+			var regex = ParseRegex;
+			regex.Replace(complexstring, m =>
+				{
+					var name = UnEscape(m.Groups["name"].Value).Trim();
+					var val = UnEscape(m.Groups["value"].Value).Trim();
+					if (val.IsEmpty()) {
+						val = EmptyValue;
+					}
+					result[name] = val;
+					return "";
+				});
+		}
+
+		private void ParseCS(string complexstring, Dictionary<string, string> result) {
+			char splitter = OptimizedParser ? ItemPrefix[0] : ItemDelimiter[0];
+			var items = complexstring.SmartSplit(false, true, splitter);
+			foreach (var item in items) {
+				var pair = item.SmartSplit(false, true, ValueDelimiter[0]);
+				var name = UnEscape(pair[0]);
+				var val = "";
+				if (pair.Count > 1) {
+					val = UnEscape(pair[1]);
+				}
+				if (val.IsEmpty()) {
+					val = EmptyValue;
+				}
+				result[name] = val;
+			}
+		}
+
+		/// <summary>
+		/// 	Возвращает значение из комплексной строки
+		/// </summary>
+		/// <param name="complexstring"> </param>
+		/// <param name="name"> </param>
+		/// <returns> </returns>
+		public string GetValue(string complexstring, string name) {
+			var dict = Parse(complexstring);
+			if (dict.ContainsKey(name)) {
+				return dict[name];
+			}
+			return NotExistedValue;
+		}
+
+		/// <summary>
+		/// 	Формирует строку
+		/// </summary>
+		/// <param name="dict"> </param>
+		/// <returns> </returns>
+		public string Generate(IDictionary<string, string> dict) {
+			if (null == dict) {
+				return string.Empty;
+			}
+			if (!string.IsNullOrEmpty(ItemDelimiter)) {
+				return GenerateCsLike(dict);
+			}
+			return GenerateTagLike(dict);
+		}
+
+		private string GenerateTagLike(IEnumerable<KeyValuePair<string, string>> dict) {
+			var result = new StringBuilder();
+			foreach (var i in dict) {
+				var name = Escape(i.Key);
+				var val = Escape(i.Value);
+				result.Append(ItemPrefix);
+				result.Append(name);
+				result.Append(ValueDelimiter);
+				result.Append(val);
+				result.Append(ItemSuffix);
+				result.Append(" ");
+			}
+			return result.ToString().Trim();
+		}
+
+		private string GenerateCsLike(IEnumerable<KeyValuePair<string, string>> dict) {
+			var result = new StringBuilder();
+			var idx = 0;
+			foreach (var i in dict) {
+				var name = Escape(i.Key);
+				var val = Escape(i.Value);
+				if (0 != idx) {
+					result.Append(ItemDelimiter);
+				}
+				result.Append(name);
+				if (val.IsNotEmpty()) {
+					result.Append(ValueDelimiter);
+					result.Append(val);
+				}
+				idx++;
+			}
+			return result.ToString();
+		}
+
+		/// <summary>
+		/// 	Создает парсер строк вида /x:val/ /y/ /z:~\~/
+		/// </summary>
+		/// <returns> </returns>
+		public static ComplexStringHelper CreateTagParser() {
 			return new ComplexStringHelper
 				{
 					ItemPrefix = "/",
@@ -58,18 +270,23 @@ namespace Qorpent.Utils {
 					ValueDelimiter = ":",
 					NotExistedValue = "",
 					EmptyValue = "",
-					ItemPrefixSubstitution = "~\\",
-					ItemSuffixSubstitution = "~\\",
-					ValueDelimiterSubstitution = "~"
+					ItemPrefixSubstitution = "`",
+					ItemSuffixSubstitution = "`",
+					ValueDelimiterSubstitution = "~",
+					OptimizedParser = true,
 				};
 		}
 
 		/// <summary>
-		/// Создает парсер строк вида <see cref="ComplexStringExtension"/>
+		/// Двух-разделительный парсер обрабатывается как сплитовой
 		/// </summary>
-		/// <returns></returns>
-		public static ComplexStringHelper CreateComplexStringParser()
-		{
+		public bool OptimizedParser  { get; set; }
+
+		/// <summary>
+		/// 	Создает парсер строк вида <see cref="ComplexStringExtension" />
+		/// </summary>
+		/// <returns> </returns>
+		public static ComplexStringHelper CreateComplexStringParser() {
 			return new ComplexStringHelper
 				{
 					ItemPrefix = "",
@@ -78,9 +295,46 @@ namespace Qorpent.Utils {
 					ValueDelimiter = ":",
 					NotExistedValue = "",
 					EmptyValue = "",
-					ItemDelimiterSubstitution = "~\\",
+					ItemDelimiterSubstitution = "`",
 					ValueDelimiterSubstitution = "~"
 				};
 		}
+
+		/// <summary>
+		/// 	Создает парсер строк вида <see cref="ComplexStringExtension" />
+		/// </summary>
+		/// <returns> </returns>
+		public static ComplexStringHelper CreateWSComplexStringParser()
+		{
+			return new ComplexStringHelper
+			{
+				ItemPrefix = "",
+				ItemSuffix = "",
+				ItemDelimiter = " ",
+				ValueDelimiter = ":",
+				NotExistedValue = "",
+				EmptyValue = "",
+				ItemDelimiterSubstitution = "`",
+				ValueDelimiterSubstitution = "~"
+			};
+		}
+
+		/// <summary>
+		/// 	Автоматически определяет тип комплексной строки
+		/// </summary>
+		/// <param name="complexString"> </param>
+		/// <returns> </returns>
+		/// <exception cref="QorpentException"></exception>
+		public static ComplexStringHelper AutoDetect(string complexString) {
+			if (complexString.Trim().StartsWith("/")) {
+				return CreateTagParser();
+			}
+			if(complexString.Contains("|")) {
+				return CreateComplexStringParser();
+			}
+			return CreateWSComplexStringParser();
+		}
+
+		private Regex _parseregex;
 	}
 }
