697ec32b14be18e44915f34658b94998e1a4fa69
remalloc <remalloc@gmail.com>
2013-04-18 16:07:50 +0600

    #ZC-538 Tests added

---
+++ Qorpent.IoC.Tests/ZC_538Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Qorpent.IoC {
+    class ZC_538Tests : ZC_538TestsBase {
+
+        [Test]
+        public void CanGetAnExtensionClassFromContainer() {
+            var extCalss = Container.Get<IExtensionClass>();
+            Assert.IsNotNull(extCalss);
+            Assert.IsInstanceOf<IExtensionClass>(extCalss);
+        }
+
+        [Test]
+        public void CanGetAHostClassFromContainer() {
+            var hostClass = Container.Get<IHostClass>();
+            Assert.IsNotNull(hostClass);
+            Assert.IsInstanceOf<IHostClass>(hostClass);
+        }
+
+        [Test]
+        public void CanInjectExtClassesToTheHostClassIfInject() {
+            var hostClass = Container.Get<IHostClass>();
+            Assert.IsNotNull(hostClass);
+            Assert.IsInstanceOf<IHostClass>(hostClass);
+            Assert.AreEqual(3, hostClass.List.Count);
+        }
+    }
+}
