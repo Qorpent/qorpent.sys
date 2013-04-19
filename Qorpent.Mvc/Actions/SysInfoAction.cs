2057f6b37cd4b248d4e1a6ce0b6b08ed2cd99097
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-04-19 13:56:03 +0600

    #AF-469 fixed - simplifyed login behavior,#AF-470 fixed _sys/info action added

---
+++ Qorpent.Mvc/Actions/SysInfoAction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace Qorpent.Mvc.Actions {
+	/// <summary>
+	/// 
+	/// </summary>
+	[Action("_sys.info",Role ="ADMIN")]
+	public class SysInfoAction :ActionBase {
+		/// <summary>
+		/// 	processing of execution - main method of action
+		/// </summary>
+		/// <returns> </returns>
+		protected override object MainProcess() {
+			var process = System.Diagnostics.Process.GetCurrentProcess();
+			return new
+				{
+					domain = GetLocalDomain(),
+					machineName = Environment.MachineName,
+					approot = EnvironmentInfo.RootDirectory,
+					pid=process.Id,
+					maxworkingset = process.MaxWorkingSet,
+					starttime = process.StartTime,
+					totalprocesstime = process.TotalProcessorTime,
+				};
+		}
+
+		/// <summary>
+		/// Вычисляет домен локальной машины
+		/// </summary>
+		/// <returns></returns>
+		public static string GetLocalDomain()
+		{
+			var ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+			if (!String.IsNullOrWhiteSpace(ipProperties.DomainName)) {
+				return ipProperties.DomainName;
+			}
+			return "local";
+		}
+	}
+}
