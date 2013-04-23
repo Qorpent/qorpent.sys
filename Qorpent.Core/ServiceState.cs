597214d7f56391c83d9ae1964fa27beb1c5ea269
remalloc <remalloc@gmail.com>
2013-04-23 14:56:53 +0600

    static class ServiceState initial commit

---
+++ Qorpent.Core/ServiceState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Qorpent {
+    /// <summary>
+    ///     State of the Service
+    /// </summary>
+    public static class ServiceState {
+        private static DateTime _startTime;
+
+        /// <summary>
+        ///     Count of current work handlers
+        /// </summary>
+        public static Int64 CurrentHandlers { get; set; }
+
+        /// <summary>
+        ///     Uptime of the service
+        /// </summary>
+        public static TimeSpan Uptime {
+            get {
+                return DateTime.Now - _startTime;
+            }
+        }
+
+        /// <summary>
+        ///     Default constructor
+        /// </summary>
+        static ServiceState() {
+            _startTime = DateTime.Now;
+        }
+    }
+}
