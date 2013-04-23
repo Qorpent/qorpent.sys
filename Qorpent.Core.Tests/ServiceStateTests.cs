ea903a4161a8887a3decbb9a1301a16e239ba33d
remalloc <remalloc@gmail.com>
2013-04-23 15:45:43 +0600

    ServiceState tests initial commit

---
+++ Qorpent.Core.Tests/ServiceStateTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Qorpent.Mvc.HttpHandler;
+
+namespace Qorpent.Core.Tests {
+    internal class ServiceEmulation : ServiceBase {
+        
+    }
+
+
+    class ServiceStateTests {
+        private ServiceEmulation _service;
+        private MvcHandler _mvcHandler;
+
+        [SetUp]
+        public void TestsSetUp() {
+            _service = new ServiceEmulation();
+        }
+
+        [Test]
+        public void CanCorrectRegisterHandlerInStatistics() {
+            Assert.AreEqual(0, ServiceState.CurrentHandlers);
+            _mvcHandler = new MvcHandler();
+            Assert.AreNotEqual(0, ServiceState.CurrentHandlers);
+        }
+    }
+}
