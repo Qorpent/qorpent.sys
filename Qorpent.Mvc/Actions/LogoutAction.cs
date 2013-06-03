0b034604895ac627a10946b0131326d35e598a64
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-06-03 12:50:53 +0600

    logout fixed

--- Qorpent.Mvc/Actions/LogoutAction.cs
+++ Qorpent.Mvc/Actions/LogoutAction.cs
@@ -50,7 +50,7 @@ namespace Qorpent.Mvc.Actions {
 			Context.SetCookie(cookie);
 			//FormsAuthentication.SignOut();
 			Context.Redirect("_sys/login.qview.qweb");
-			return null;
+			return true;
 		}
 	}
 }
