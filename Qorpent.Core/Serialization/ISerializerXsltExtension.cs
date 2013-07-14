82b2520c896da3a0bab1c4e244a207ddd893527e
comdiv <fagim.sadykov@gmail.com>
2013-07-14 23:29:57 +0600

    #Q-17 fixed

---
+++ Qorpent.Core/Serialization/ISerializerXsltExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace Qorpent.Serialization {
+    /// <summary>
+    /// Интерфейс расширения XSLT для сериализатора (Q-17)
+    /// </summary>
+    public interface ISerializerXsltExtension {
+        /// <summary>
+        /// Пространство имен для расширения
+        /// </summary>
+        /// <returns></returns>
+        string GetNamespace();
+        /// <summary>
+        /// Рекомендованный префикс для расширения
+        /// </summary>
+        /// <returns></returns>
+        string GetPrefix();
+    }
+}
