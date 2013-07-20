f983d62478e739ba1d9978645728533707e16c12
comdiv <fagim.sadykov@gmail.com>
2013-07-20 19:14:06 +0600

    factory interface fix

--- Qorpent.Core/IoC/IFactory.cs
+++ Qorpent.Core/IoC/IFactory.cs
@@ -5,11 +5,12 @@ namespace Qorpent.IoC {
     /// Описывает фабрику по умолчанию, в качестве страховки контейнера (#QPT-80)
     /// </summary>
     public interface IFactory {
-        /// <summary>
-        /// Реализует фабричную логику для указанного типа, тип возвращаемого значения должен быть верным
-        /// </summary>
-        /// <param name="serviceType"></param>
-        /// <returns></returns>
-        object Get(Type serviceType);
+	    /// <summary>
+	    /// Реализует фабричную логику для указанного типа, тип возвращаемого значения должен быть верным
+	    /// </summary>
+	    /// <param name="serviceType"></param>
+	    /// <param name="name"></param>
+	    /// <returns></returns>
+	    object Get(Type serviceType,string name = "");
     }
 }
