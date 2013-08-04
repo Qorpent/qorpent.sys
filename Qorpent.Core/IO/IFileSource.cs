c67f9ab948988b322c7cb01289990fa60db6d651
comdiv <fagim.sadykov@gmail.com>
2013-08-05 02:38:56 +0600

    file source interfaces added and zip source realized

---
+++ Qorpent.Core/IO/IFileSource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qorpent.IO
+{
+	/// <summary>
+	/// Источник файлов
+	/// </summary>
+	public interface IFileSource {
+		/// <summary>
+		/// Перечисляет имена файлов
+		/// </summary>
+		/// <returns></returns>
+		IEnumerable<string> GetFileNames();
+		/// <summary>
+		/// Открывает поток на чтение
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		Stream Open(string name);
+	}
+}
---
+++ Qorpent.IO/Zip/ZipFileSource.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Qorpent.IoC;
+
+namespace Qorpent.IO.Zip
+{
+	/// <summary>
+	/// Источник файлов из Zip-архива
+	/// </summary>
+	[ContainerComponent(Name="zip.file.source",ServiceType = typeof(IFileSource))]
+	public class ZipFileSource : IFileSource
+	{
+		private Package _package;
+
+		/// <summary>
+		/// Загружает Zip - исходник из потока
+		/// </summary>
+		/// <param name="stream"></param>
+		public ZipFileSource(Stream stream) {
+			this._package = Package.Open(stream);
+		}
+
+		/// <summary>
+		/// Загружает Zip-источник из ранее подготовленного потока
+		/// </summary>
+		/// <param name="package"></param>
+		public ZipFileSource(Package package) {
+			this._package = package;
+		}
+		/// <summary>
+		/// Возвращает имена файлов из Zip- файла
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<string> GetFileNames() {
+			return _package.GetParts().Select(_ => _.Uri.ToString()).ToArray();
+		}
+		/// <summary>
+		/// Открывает поток файла на чтение из Zip-файла
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		public Stream Open(string name) {
+			return _package.GetPart(new Uri(name,UriKind.Relative)).GetStream(FileMode.Open);
+		}
+	}
+}
