e3c5f79c996c24b549313ba9281d331aab286cf0
comdiv <fagim.sadykov@gmail.com>
2013-08-06 11:53:17 +0600

    #Q-86 fixed

---
+++ Qorpent.Core/IoC/ComponentDefinitionWrapper.cs
@@ -0,0 +1,124 @@
+using System.Collections.Generic;
+using Qorpent.Serialization;
+
+namespace Qorpent.IoC {
+	/// <summary>
+	/// Специальная сериализуемая обертка над компонентом контейнера
+	/// </summary>
+	[Serialize]
+	public class ComponentDefinitionWrapper {
+		/// <summary>
+		/// Создает обертку
+		/// </summary>
+		/// <param name="c"></param>
+		public ComponentDefinitionWrapper(IComponentDefinition c) {
+			Name = c.Name;
+			var impltype = c.Implementation == null
+				               ? c.ImplementationType
+				               : c.Implementation.GetType();
+			ImplAssembly = impltype.Assembly.GetName().Name;
+			ImplNs = impltype.Namespace;
+			ImplType = impltype.Name;
+
+			ServiceAssembly = c.ServiceType.Assembly.GetName().Name;
+			ServiceNs = c.ServiceType.Namespace;
+			ServiceType = c.ServiceType.Name;
+			Lifestyle = c.Lifestyle;
+			Tag = c.Tag;
+			Role = c.Role;
+			Priority = c.Priority;
+			ActivationCount = c.ActivationCount;
+			CreationCount = c.CreationCount;
+			Parametes = c.Parameters;
+			Help = c.Help;
+			Id = c.ContainerId;
+		}
+		/// <summary>
+		/// Номер в контейнере
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Помощь по компоненту
+		/// </summary>
+		[SerializeNotNullOnly]
+		public string Help { get; set; }
+
+		/// <summary>
+		/// Счетчик созданных объектов
+		/// </summary>
+		[SerializeNotNullOnly]
+		public int CreationCount { get; set; }
+		/// <summary>
+		/// Счетчик активаций
+		/// </summary>
+		[SerializeNotNullOnly]
+		public int ActivationCount { get; set; }
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[SerializeNotNullOnly]
+		public IDictionary<string, object> Parametes { get; set; }
+
+		/// <summary>
+		/// Имя компонента
+		/// </summary>
+		[SerializeNotNullOnly]
+		public int Priority { get; set; }
+		/// <summary>
+		/// Имя компонента
+		/// </summary>
+		[SerializeNotNullOnly]
+		public string Role { get; set; }
+		/// <summary>
+		/// Имя компонента
+		/// </summary>
+		[SerializeNotNullOnly]
+		public string Tag { get; set; }
+
+		/// <summary>
+		/// Цикл жизни
+		/// </summary>
+		[Serialize]
+		public Lifestyle Lifestyle { get; set; }
+
+		/// <summary>
+		/// Имя компонента
+		/// </summary>
+		[SerializeNotNullOnly]
+		public string ServiceType { get; set; }
+		/// <summary>
+		/// Имя компонента
+		/// </summary>
+		[SerializeNotNullOnly]
+		public string ServiceNs { get; set; }
+		/// <summary>
+		/// Имя компонента
+		/// </summary>
+		[SerializeNotNullOnly]
+		public string ServiceAssembly { get; set; }
+		/// <summary>
+		/// Имя компонента
+		/// </summary>
+		[SerializeNotNullOnly]
+		public string ImplType { get; set; }
+		/// <summary>
+		/// Имя компонента
+		/// </summary>
+		[SerializeNotNullOnly]
+		public string ImplNs { get; set; }
+		/// <summary>
+		/// Имя компонента
+		/// </summary>
+		[SerializeNotNullOnly]
+		public string ImplAssembly { get; set; }
+
+		/// <summary>
+		/// Имя компонента
+		/// </summary>
+		[SerializeNotNullOnly]
+		public string Name { get; set; }
+	}
+}
