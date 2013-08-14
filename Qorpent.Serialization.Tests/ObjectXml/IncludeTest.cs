d0712214ef23c9d6c883b46371d32b323b24cf0c
comdiv <fagim.sadykov@gmail.com>
2013-08-14 19:50:27 +0600

    include and partial tests prepared

---
+++ Qorpent.Serialization.Tests/ObjectXml/IncludeTest.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Qorpent.Utils.Extensions;
+
+namespace Qorpent.Serialization.Tests.ObjectXml {
+	[TestFixture]
+	public class IncludeTest : CompileTestBase {
+		[Test]
+		public void CanInclude()
+		{
+			var code = @"
+class A x=1
+	test '${x}'
+class B x=2
+	include A
+";
+			var result = Compile(code).Get("B");
+			Assert.AreEqual(1, result.Compiled.Descendants("class").Count());
+			Assert.AreEqual("1", result.Compiled.Descendants("test").First().Attr("code"));
+		}
+
+		[Test]
+		public void CanIncludeBodyOnly()
+		{
+			var code = @"
+class A x=1
+	test '${x}'
+class B x=2
+	include A body
+";
+			var result = Compile(code).Get("B");
+			Assert.AreEqual(0, result.Compiled.Descendants("class").Count());
+			Assert.AreEqual("1", result.Compiled.Descendants("test").First().Attr("code"));
+		}
+
+		[Test]
+		public void CanEmbed()
+		{
+			var code = @"
+class A x=1
+	test '${x}%{x}'
+class B x=2
+	include A
+";
+			var result = Compile(code).Get("B");
+			Assert.AreEqual(1, result.Compiled.Descendants("class").Count());
+			Assert.AreEqual("12", result.Compiled.Descendants("test").First().Attr("code"));
+		}
+
+
+		[Test]
+		public void CanIncludeRecursivelly()
+		{
+			var code = @"
+class A x=1 
+	test '${x}'
+class B x=2
+	include A
+	test '${x}'
+class C x=3
+	include B
+	test '${x}'
+";
+			var result = Compile(code).Get("C");
+			Assert.AreEqual(2, result.Compiled.Descendants("class").Count());
+			CollectionAssert.AreEquivalent(new[]{"1","2","3"},
+				result.Compiled.Descendants("test").Select(_=>_.Attr("code"))
+				);			
+		}
+	}
+}
---
+++ Qorpent.Serialization.Tests/ObjectXml/PartialClassSupport.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using Qorpent.Utils.Extensions;
+
+namespace Qorpent.Serialization.Tests.ObjectXml {
+	[TestFixture]
+	public class PartialClassSupport : CompileTestBase {
+		[Test]
+		public void CanDefineClassFromOverride() {
+			var code = @"
+~class A
+";
+			var result = Compile(code).Get("A");
+			Assert.NotNull(result);
+		}
+
+		[Test]
+		public void CanDefineClassFromExtension()
+		{
+			var code = @"
++class A
+";
+			var result = Compile(code).Get("A");
+			Assert.NotNull(result);
+		}
+
+		[Test]
+		public void CanOverrideAttribute()
+		{
+			var code = @"
+class A x=1 y=1
+~class A x=2
+";
+			var result = Compile(code).Get("A");
+			Assert.AreEqual("2",result.Compiled.Attr("x"));
+		}
+
+		[Test]
+		public void CanExtendAttribute()
+		{
+			var code = @"
+class A x=1 y=1
++class A x=2 z=2
+";
+			var result = Compile(code).Get("A");
+			Assert.AreEqual("2", result.Compiled.Attr("z"));
+			Assert.AreEqual("1", result.Compiled.Attr("x"));
+		}
+
+		[Test]
+		public void CanOverrideValidOrder()
+		{
+			var code = @"
+class A x=1 y=1
+~class A x=3 priority=20
+~class A x=2 priority=10
+";
+			var result = Compile(code).Get("A");
+			Assert.AreEqual("3", result.Compiled.Attr("x"));
+		}
+
+		[Test]
+		public void CanExtendValidOrder()
+		{
+			var code = @"
+class A x=1 y=1
++class A z=3 u=4 priority=20
++class A z=4 w=2 priority=10
+";
+			var result = Compile(code).Get("A");
+			Assert.AreEqual("4", result.Compiled.Attr("u"));
+			Assert.AreEqual("4", result.Compiled.Attr("z"));
+		}
+
+		[Test]
+		public void CanExtendAndOverrideValidOrder()
+		{
+			var code = @"
+class A x=1 y=1
++class A z=3 u=4 w=3 r=2 priority=20
++class A z=4 w=2 priority=10
+~class A z=2 priority=20
+~class A z=1 u=3 priority=10
+";
+			var result = Compile(code).Get("A");
+			Assert.AreEqual("3", result.Compiled.Attr("u"));
+			Assert.AreEqual("2", result.Compiled.Attr("r"));
+			Assert.AreEqual("2", result.Compiled.Attr("w"));
+			Assert.AreEqual("2", result.Compiled.Attr("z"));
+		}
+	}
+}
