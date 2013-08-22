350d0383107ba3bc5dc0e9e090a44e2dbfa10ddd
MakssUR <maksimurakv@gmail.com>
2013-08-22 12:54:31 +0600

    add new metods

--- Qorpent.Mvc/Actions/ListFilesAction.cs
+++ Qorpent.Mvc/Actions/ListFilesAction.cs
@@ -9,48 +9,80 @@ namespace Qorpent.Mvc.Actions
     /// <summary>
     /// 	Возвращает текущий откомпилированный манифест (полный XML)
     /// </summary>
-    [Action("_sys.listfiles", Role = "DEVELOPER", Help = "!!!!Возвращает список всех файлов и папок, где находися приложение", Arm = "admin")]
+    [Action("_sys.listfiles", Role = "DEVELOPER", Help = "Возвращает список всех файлов и папок, где находися приложение", Arm = "admin")]
+    public class DirectoryEntry
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ID { get; set; }
+        
+        /// <summary>
+        /// Имя папки или файла
+        /// </summary>
+       
+        public string ObjName { get; set; }
+        /// <summary>
+        /// Тайп (папка или файл)
+        /// </summary>
+        public string ObjType { get; set; }
+    }
+
+    /// <summary>
+    /// Методы
+    /// </summary>
+    public class MetodsCollect
+    {
+        /// <summary>
+        /// Метод вовращает список дирректориев
+        /// </summary>
+        /// <returns></returns>
+        public static DirectoryInfo[] ListDir()
+        {
+            var di = new DirectoryInfo(Environment.CurrentDirectory);
+            return di.GetDirectories();
+        }
+        
+        /// <summary>
+        /// Список Файлов
+        /// </summary>
+        /// <returns></returns>
+        public static string[] ListFile()
+        {
+            return Directory.GetFiles(Environment.CurrentDirectory);
+        }
+
+        /// <summary>
+        /// Добавляетв в коллекцию
+        /// </summary>
+        /// <returns></returns>
+        public static List<DirectoryEntry> AddListFilesAndDirs()
+        {
+            var listFilesCollection = new List<DirectoryEntry>();
+            for (int index = 0; index < ListDir().Count(); index++)
+            {
+                listFilesCollection.Add(new DirectoryEntry() { ID = index + 1, ObjName = ListDir()[index].ToString(), ObjType = "Dir" });
+            }
+            for (var index = 0; index < ListFile().Count(); index++)
+            {
+                listFilesCollection.Add(new DirectoryEntry() { ID = index + 1 + ListDir().Count(), ObjName = ListFile()[index], ObjType = "File" });
+            }
+            return listFilesCollection;
+        }
+    }    
+
+
+    /// <summary>
+    /// r
+    /// </summary>
     public class ListFilesAction : ActionBase
     {
-
         /// <summary>
         /// 	fh
         /// </summary>
         protected override object MainProcess()
         {
-            var dirs = ListDirectrory();
-            var files = ListFiles();
-
-            var result = new List<string>();
-
-            result.AddRange(dirs);
-            result.AddRange(files);
-
-            return result;
-        }
-
-        private IEnumerable<string> ListDirectrory()
-        {
-            var di = new DirectoryInfo(EnvironmentInfo.RootDirectory);
-            if (null == di)
-            {
-                throw new Exception("Cannot get directory info");
-            }
-
-            return di.GetDirectories().Select(directoryInfo => directoryInfo.Name).ToList();
-        }
-
-
-        private IEnumerable<string> ListFiles()
-        {
-            string[] di = Directory.GetFiles(EnvironmentInfo.RootDirectory); 
-            
-            if (null == di)
-            {
-                throw new Exception("Cannot get directory info");
-            }
-            return di;
-            
+            return MetodsCollect.AddListFilesAndDirs();
         }
     }
 }
