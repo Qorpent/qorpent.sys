172b3177eeccb364d3c860c500455a8285dbec76
comdiv <fagim.sadykov@gmail.com>
2013-08-24 23:28:31 +0600

    #Q-115 InProgress main class set and condition implemented

---
+++ Qorpent.Serialization/BSharp/Matcher/SingleCondition.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Qorpent.Utils.Extensions;
+
+namespace Qorpent.BSharp.Matcher {
+	/// <summary>
+	/// Единичное условие
+	/// </summary>
+	public sealed class SingleCondition {
+		private XElement _e;
+
+		/// <summary>
+		/// Создает условие из атрибута
+		/// </summary>
+		/// <param name="a"></param>
+		public SingleCondition(XAttribute a) {
+			Value = a.Value;
+			ConditionType = ConditionType.Eq;
+			if ("NULL" == Value) {
+				ConditionType = ConditionType.IsNull;
+			}
+			AttributeName = a.Name.LocalName;
+			if (a.Name.LocalName.Contains("__")) {
+				SetupFromSpecialName(a);
+			}
+			if (AttributeName == "xpath") {
+				ConditionType = ConditionType.XPath;
+			}
+		}
+
+		private void SetupFromSpecialName(XAttribute a) {
+			var n = a.Name.LocalName;
+			if (n.Contains("__EXC__")) {
+				Negate = true;
+				n = n.Replace("__EXC__", "");
+			}
+			if (Value == "NULL") {
+				ConditionType= ConditionType.IsNull;
+			}else if (Value == "TRUE") {
+				ConditionType =ConditionType.IsTrue;
+			}
+			if (n.EndsWith("__GT____GT__")) {
+				ConditionType = ConditionType.Gr;
+			}else if (n.EndsWith("__GT__")) {
+				ConditionType = ConditionType.GrE;
+			}else if (n.EndsWith("__LT____LT__")) {
+				ConditionType= ConditionType.Le;
+			}else if (n.EndsWith("__LT__")) {
+				ConditionType = ConditionType.LeE;
+			}else if (n.EndsWith("__TILD__")) {
+				ConditionType = ConditionType.Match;
+			}else if (n.EndsWith("__AMP__")){
+				ConditionType = ConditionType.InList;
+			}else if (n.EndsWith("__PERC__")) {
+				ConditionType = ConditionType.Contains;
+			}
+			var idx = n.IndexOf("__");
+			if (-1 != idx) {
+				n = n.Substring(0, idx);
+			}
+			AttributeName = n;
+			
+		}
+
+		/// <summary>
+		/// Имя атрибута
+		/// </summary>
+		public string AttributeName { get; set; }
+		/// <summary>
+		/// Тип условия
+		/// </summary>
+		public ConditionType ConditionType { get; set; }
+		/// <summary>
+		/// Отрицание
+		/// </summary>
+		public bool Negate { get; set; }
+		/// <summary>
+		/// Значение
+		/// </summary>
+		public string Value { get; set; }
+
+		/// <summary>
+		/// Проверка условия на целевом элементе
+		/// </summary>
+		/// <param name="e"></param>
+		/// <returns></returns>
+		public bool IsMatch(XElement e) {
+			_e = e;
+			var a = e.Attribute(AttributeName);
+			var mainismatch = InternalIsMatch(a);
+			return Negate? !mainismatch : mainismatch;
+		}
+
+		private bool InternalIsMatch(XAttribute a) {
+			
+			switch (ConditionType) {
+				case ConditionType.Contains:
+					if (null == a) return false;
+					return a.Value.Contains(Value);
+				case ConditionType.Eq:
+					if (null == a) return string.IsNullOrWhiteSpace(Value);
+					return Value == a.Value;
+				case ConditionType.Gr:
+					if (null == a) return 0 > Value.ToDecimal();
+					return a.Value.ToDecimal() > Value.ToDecimal();
+				case ConditionType.GrE:
+					if (null == a) return 0 >= Value.ToDecimal();
+					return a.Value.ToDecimal() >= Value.ToDecimal();
+				case ConditionType.Le:
+					if (null == a) return 0 < Value.ToDecimal();
+					return a.Value.ToDecimal() < Value.ToDecimal();
+				case ConditionType.LeE:
+					if (null == a) return 0 <= Value.ToDecimal();
+					return a.Value.ToDecimal() <= Value.ToDecimal();
+				case ConditionType.InList:
+					if (null == a) return false;
+					return a.Value.SmartSplit(false, true, ' ', ',', ';').Contains(Value);
+				case ConditionType.IsTrue:
+					if (null == a) return false;
+					return a.Value.ToBool();
+				case ConditionType.IsNull:
+					return a == null;
+				case ConditionType.Match:
+					if (null == a) return false;
+					string a1 = a.Value;
+					if (string.IsNullOrWhiteSpace(a1)) return false;
+					return Regex.IsMatch(a1, Value);
+				case ConditionType.XPath:
+					return _e.XPathSelectElements(Value).Any();
+				default:
+					throw new Exception("unknown condition " + ConditionType);
+
+			}
+		}
+	}
+}
