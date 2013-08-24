c97b1452064643e0182c5d8bf3f19e721ff69e68
comdiv <fagim.sadykov@gmail.com>
2013-08-24 19:52:35 +0600

    #Q-114 in progress test prepared

--- Qorpent.Serialization.Tests/BSharp/ConditionalInternals.cs
+++ Qorpent.Serialization.Tests/BSharp/ConditionalInternals.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 using NUnit.Framework;
 using Qorpent.Utils.Extensions;
 namespace Qorpent.Serialization.Tests.BSharp {
