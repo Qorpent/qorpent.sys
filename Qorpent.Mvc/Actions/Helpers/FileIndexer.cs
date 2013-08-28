af8f44819ece1c79352842617ef4d710825bd79f
MakssUR <maksimurakv@gmail.com>
2013-08-28 18:59:18 +0600

    add out data

--- Qorpent.Mvc/Actions/Helpers/FileIndexer.cs
+++ Qorpent.Mvc/Actions/Helpers/FileIndexer.cs
@@ -1,29 +1,43 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Qorpent.Mvc.Actions.Helpers {
 	/// <summary>
 	/// Индексатор файлов
 	/// </summary>
 	public class FileIndexer {
-		/// <summary>
-		/// Выводит перечень файолов и папок приложения по маске
-		/// </summary>
-		/// <param name="fileMask"></param>
-		/// <param name="showDirs"></param>
-		/// <param name="showFiles"></param>
-		/// <returns></returns>
-		/// <exception cref="NotImplementedException"></exception>
-		public IEnumerable<FileListEntry> Collect(string fileMask=null, bool showDirs=true, bool showDirs =true) 
+	    /// <summary>
+	    /// Выводит перечень файолов и папок приложения по маске
+	    /// </summary>
+	    /// <param name="fileMask"></param>
+	    /// <param name="showDirs"></param>
+	    /// <param name="showFiles"></param>
+	    /// <returns></returns>
+	    /// <exception cref="NotImplementedException"></exception>
+	    public IEnumerable<FileListEntry> Collect(string fileMask = null, bool showDirs = true, bool showFiles = true) 
         {
             var listFilesCollection = new List<FileListEntry>();
-            if (showDirs)
+            var filesNameMass = ListFile();
+            var dirNameMass = ListDir();
+	       if (showDirs)
 		    {
+                for (int index = 0; index < filesNameMass.Count(); index++)
+                {
+                    listFilesCollection.Add(new FileListEntry() { LocalPath = filesNameMass[index], Type = FileListEntryType.Directory });
+                }
 		    }
-            if (showDirs)
+            
+          // var dirsNameMass = ListDir();
+            if (showFiles)
             {
+                for (int index = 0; index < dirNameMass.Count(); index++)
+                {
+                    listFilesCollection.Add(new FileListEntry() { LocalPath = dirNameMass[index].ToString(), Type = FileListEntryType.File });
+                }
             }
+	        return listFilesCollection;
         }
         /// <summary>
         /// Список папок
