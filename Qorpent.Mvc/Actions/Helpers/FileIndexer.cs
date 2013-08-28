bc87cfd55768b6253fd13d661dcf44baae45babe
MakssUR <maksimurakv@gmail.com>
2013-08-28 17:42:18 +0600

    start new vers

--- Qorpent.Mvc/Actions/Helpers/FileIndexer.cs
+++ Qorpent.Mvc/Actions/Helpers/FileIndexer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Qorpent.Mvc.Actions.Helpers {
 	/// <summary>
@@ -14,8 +15,33 @@ namespace Qorpent.Mvc.Actions.Helpers {
 		/// <param name="showFiles"></param>
 		/// <returns></returns>
 		/// <exception cref="NotImplementedException"></exception>
-		public IEnumerable<FileListEntry> Collect(string fileMask=null, bool showDirs=true, bool showFiles =true) {
-			throw new NotImplementedException();
-		}
+		public IEnumerable<FileListEntry> Collect(string fileMask=null, bool showDirs=true, bool showDirs =true) 
+        {
+            var listFilesCollection = new List<FileListEntry>();
+            if (showDirs)
+		    {
+		    }
+            if (showDirs)
+            {
+            }
+        }
+        /// <summary>
+        /// Список папок
+        /// </summary>
+        /// <returns></returns>
+        public DirectoryInfo[] ListDir()
+        {
+            var di = new DirectoryInfo(EnvironmentInfo.RootDirectory);
+            return di.GetDirectories("*.*", SearchOption.AllDirectories);
+        }
+        /// <summary>
+        /// Список файлов
+        /// </summary>
+        /// <returns></returns>
+        public string[] ListFile()
+        {
+            return Directory.GetFiles(EnvironmentInfo.RootDirectory, "*.*", SearchOption.AllDirectories);
+        }
+
 	}
 }
