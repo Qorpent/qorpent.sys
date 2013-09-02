0389e047aa93d2ddb16ad1d3157ad834f0878fc9
comdiv <fagim.sadykov@gmail.com>
2013-09-02 10:00:18 +0600

    files for dot added

---
+++ Qorpent.Serialization/Dot/DotConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace Qorpent.Dot {
+	/// <summary>
+	/// Набор констант дота
+	/// </summary>
+	public static class DotConstants {
+		/// <summary>
+		/// Атрибут заголовка
+		/// </summary>
+		public const string LabelAttribute = "label";
+		/// <summary>
+		/// Атрибут направления разрисовки графа
+		/// </summary>
+		public const string RankDirAttribute = "rankdir";
+		/// <summary>
+		/// Атрибут формы узла
+		/// </summary>
+		public const string ShapeAttribute = "shape";
+	}
+}
---
+++ Qorpent.Serialization/Dot/NodeShapeType.cs
@@ -0,0 +1,11 @@
+﻿namespace Qorpent.Dot {
+	/// <summary>
+	/// Типы форм узлов
+	/// </summary>
+	public enum NodeShapeType {
+		/// <summary>
+		/// Прямоугольник
+		/// </summary>
+		Box,
+	}
+}
