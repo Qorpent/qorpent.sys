fc1a8e506851f90cff960ccb8f12500057e431a4
Larisa Sadykova <larisatyan@gmail.com>
2013-09-04 15:52:00 +0600

    стиль графов, границ и узлов

---
+++ Qorpent.Serialization/Dot/StyleGraphType.cs
@@ -0,0 +1,41 @@
+﻿namespace Qorpent.Dot {
+	/// <summary>
+	/// Стили узлов
+	/// </summary>
+	public enum StyleGraphType {
+		/// <summary>
+		/// Сплошная граница графа
+		/// </summary>
+        Solid,
+        /// <summary>
+        /// Пунктирная граница графа 
+		/// </summary>
+        Dashed,
+        /// <summary>
+        /// Точечная граница графа 
+        /// </summary>
+        Dotted,
+        /// <summary>
+        /// Сплошная жирная граница графа
+        /// </summary>
+        Bold,
+        /// <summary>
+        /// Округляет углы границ графа
+        /// </summary>
+        Rounded,
+        /// <summary>
+        /// Заполнение тела графа (серым по умолчанию)
+        /// </summary>
+        Filled,
+        /// <summary>
+        /// На три сектора разбито тело графа, но у меня не воспроизводится....????
+        /// </summary>
+        Striped,
+        
+        
+
+
+
+
+	}
+}
