e6069cb36b514e32a470f177485b6bd7e9b4435e
Dmitry Shamrikov <bivis08@gmail.com>
2013-09-11 15:54:43 +0600

    XmlEscaper class added

---
+++ Qorpent.Utils.Tests/XmlEscaperTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Qorpent.Utils.Extensions;
+
+
+namespace Qorpent.Utils.Tests
+{
+    [TestFixture]
+    public class XmlEscaperTests
+    {
+        [Test]
+        public void EscapeTest()
+        {
+            String bxl = "~";
+            String xml = "__TILD__";
+            Assert.AreEqual(XmlEscaper.Escape(bxl), xml);
+        }
+
+        [Test]
+        public void UnescapeTest()
+        {
+            String xml = "__PERC__";
+            String bxl = "%";
+            Assert.AreEqual(XmlEscaper.Unescape(xml), bxl);
+        }
+    }
+}
---
+++ Qorpent.Utils/Extensions/XmlEscaper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qorpent.Utils.Extensions
+{
+    /// <summary>
+    /// преобразовывает спецсимволы из xml в bxl и обратно
+    /// </summary>
+    public class XmlEscaper
+    {
+        private static readonly Dictionary<String, String> _xml = new Dictionary<string, string>()
+            {
+                {"+", "__PLUS__"},
+                {"?", "__ASK__"},
+                {"!", "__EXC__"},
+                {"~", "__TILD__"},
+                {"@", "__AT__"},
+                {"*", "__STAR__"},
+                {"$", "__USD__"},
+                {"^", "__UP__"},
+                {"&", "__AMP__"},
+                {"/", "__DIV__"},
+                {":", "__DBL__"},
+                {"%", "__PERC__"},
+                {"(", "__LBRACE__"},
+                {"},", "__RBRACE__"},
+                {"[", "__LINDEX__"},
+                {"]", "__RINDEX__"},
+                {"{", "__LBLOCK__"},
+                {"}", "__RBLOCK__"},
+                {"|", "__VLINE__"},
+                {";", "__PERIOD__"},
+                {"<", "__LT__"},
+                {">", "__GT__"}
+            };
+
+        private static readonly Dictionary<String, String> _bxl = new Dictionary<string, string>()
+            {
+                {"__PLUS__", "+"},
+                {"__ASK__", "?"},
+                {"__EXC__", "!"},
+                {"__TILD__", "~"},
+                {"__AT__", "@"},
+                {"__STAR__", "*"},
+                {"__USD__", "$"},
+                {"__UP__", "^"},
+                {"__AMP__", "&"},
+                {"__DIV__", "/"},
+                {"__DBL__", ":"},
+                {"__PERC__", "%"},
+                {"__LBRACE__", "("},
+                {"__RBRACE__", "},"},
+                {"__LINDEX__", "["},
+                {"__RINDEX__", "]"},
+                {"__LBLOCK__", "{"},
+                {"__RBLOCK__", "}"},
+                {"__VLINE__", "|"},
+                {"__PERIOD__", ";"},
+                {"__LT__", "<"},
+                {"__GT__", ">"}
+            };
+
+        /// <summary>
+        /// Converts specials symbols to equivalent string (e.g. "+" => "__PLUS__")
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static String Escape(String c)
+        {
+            String r = c;
+            if (_xml.TryGetValue(c, out r))
+                return r;
+            return c;
+        }
+
+        /// <summary>
+        /// Convert String representation to special symbols (e.g. "__PLUS__" => "+")
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static String Unescape(String c)
+        {
+            String r = c;
+            if (_bxl.TryGetValue(c, out r))
+                return r;
+            return c;
+        }
+    }
+}
