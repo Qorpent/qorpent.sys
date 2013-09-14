cd687ac85f1704625168e06780866cb0ed3b8bc6
comdiv <fagim.sadykov@gmail.com>
2013-09-15 02:23:40 +0600

    HTML5 compatibility for Html render improved, user-friendly serialization extensions realized

---
+++ Qorpent.Serialization.Tests/XHtml5XmlWriterTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using NUnit.Framework;
+
+namespace Qorpent.Serialization.Tests
+{
+    /// <summary>
+    /// Проверяет работоспособность XHtml5 XML Writer
+    /// </summary>
+    [TestFixture]
+    public class XHtml5XmlWriterTests
+    {
+        [Test]
+        public void WroteElementsCorrectlyClosed() {
+            var sw = new StringWriter();
+            var xw = new XHtml5XmlWriter(sw);
+            xw.GenerateHtmlWrapper = false;
+            xw.WriteStartElement("meta");
+            xw.WriteEndElement();
+            xw.WriteStartElement("p"); 
+            xw.WriteEndElement();
+            xw.WriteStartElement("img");
+            xw.WriteEndElement();
+            xw.WriteStartElement("img");
+            xw.WriteString("test");
+            xw.WriteEndElement();
+            var res = sw.ToString();
+            Console.WriteLine(res);
+            Assert.AreEqual("<meta /><p></p><img /><img>test</img>",res);
+        }
+
+        [Test]
+        public void CanWrapXElementConent()
+        {
+            var sw = new StringWriter();
+            var xw = new XHtml5XmlWriter(sw);
+            var xe = new XElement("result", new XAttribute("code","1"),"test");
+            xe.WriteTo(xw);
+            var res = sw.ToString();
+            Console.WriteLine(res);
+            Assert.AreEqual(@"<!DOCTYPE html><html><head><meta name=""generator"" value=""XHtml5XmlWriter"" /></head><body><result code=""1"">test</result><style type=""text/css"">result {border:solid 1px black; padding:1px; margin:1px;}result:before {content: ""[result:"" attr(code) ""] ""}</style></body></html>", res);
+        }
+        [Test]
+        public void CanPreventAutoStyling()
+        {
+            var sw = new StringWriter();
+            var xw = new XHtml5XmlWriter(sw);
+            xw.GenerateStylesForNonHtmlElements = false;
+            var xe = new XElement("result", new XAttribute("code", "1"), "test");
+            xe.WriteTo(xw);
+            var res = sw.ToString();
+            Console.WriteLine(res);
+            Assert.AreEqual(@"<!DOCTYPE html><html><head><meta name=""generator"" value=""XHtml5XmlWriter"" /></head><body><result code=""1"">test</result></body></html>", res);
+        }
+
+        [Test]
+        public void CanWrapXElementConentWithHtmlDefinition()
+        {
+            var sw = new StringWriter();
+            var xw = new XHtml5XmlWriter(sw);
+            var xe = new XElement("result", new XAttribute("code", "1"), "test");
+            var be = new XElement("body", xe);
+            var he = new XElement("html",be);
+            he.WriteTo(xw);
+            var res = sw.ToString();
+            Console.WriteLine(res);
+            Assert.AreEqual(@"<!DOCTYPE html><html><body><result code=""1"">test</result><style type=""text/css"">result {border:solid 1px black; padding:1px; margin:1px;}result:before {content: ""[result:"" attr(code) ""] ""}</style></body></html>", res);
+        }
+    }
+}
--- Qorpent.Serialization/Serialization/DotScriptSerializer.cs
+++ Qorpent.Serialization/Serialization/DotScriptSerializer.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using Qorpent.Graphs;
 using Qorpent.IoC;
 
 namespace Qorpent.Serialization {
@@ -7,6 +9,21 @@ namespace Qorpent.Serialization {
     /// </summary>
     [ContainerComponent(Lifestyle.Transient, ServiceType = typeof(ISerializer), Name = "dot.serializer")]
     public class DotScriptSerializer:ISerializer {
+        private readonly GraphOptions _options;
+
+        /// <summary>
+        /// Простой конструктор
+        /// </summary>
+        public DotScriptSerializer() {
+            
+        }
+        /// <summary>
+        /// Создает сериализатор в привязке к опциям генерации графа
+        /// </summary>
+        /// <param name="options"></param>
+        public DotScriptSerializer(GraphOptions options) {
+            _options = options;
+        }
         /// <summary>
         /// Сериализует переданный объект в текстовой поток
         /// </summary>
@@ -19,7 +36,24 @@ namespace Qorpent.Serialization {
         /// не является типовым сценарием для коммутриуемх API
         /// </remarks>
         public void Serialize(string name, object value, TextWriter output) {
-            throw new System.NotImplementedException();
+            if (null == value) {
+                output.Write("digraph NULL{}");
+                return;
+            }
+            if (value is string) {
+                output.Write((string)value);
+                return;
+            }
+            if (value is IGraphConvertible) {
+                output.Write( ((IGraphConvertible)value).GenerateGraphScript(_options));
+                return;
+            }
+            if (value is IGraphSource)
+            {
+                output.Write(((IGraphSource)value).BuildGraph(_options).GenerateGraphScript());
+                return;
+            }
+            throw  new Exception("given object cannot be represented as DOT");
         }
     }
 }
