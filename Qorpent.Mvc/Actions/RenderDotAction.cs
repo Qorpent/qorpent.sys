db97c51a17be3dfd8a285bf6d4f8044e57c609a5
comdiv <fagim.sadykov@gmail.com>
2013-09-16 18:25:19 +0600

    wiki_fix

---
+++ Qorpent.Mvc/Actions/RenderDotAction.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Linq;
+using Qorpent.Graphs;
+using Qorpent.IoC;
+using Qorpent.Mvc.Binding;
+
+namespace Qorpent.Mvc.Actions
+{
+    /// <summary>
+    /// Действие отрисовки скрипта на DOT в виде SVG
+    /// </summary>
+    [Action("_sys.renderdot")]
+    public class RenderDotAction:ActionBase
+    {
+        [Bind(Required = true,IsLargeText = true)]private string Script { get; set; }
+        [Inject(Name = "dot.graph.provider",Required = true)]private IGraphProvider Provider { get; set; }
+        /// <summary>
+        /// Возвращает XML, содержащий SVG с графиком
+        /// </summary>
+        /// <returns></returns>
+        protected override object MainProcess() {
+            if (!Script.Contains("digraph")) {
+                Script = "digraph G{\r\n" + Script + "\r\n}";
+            }
+            return XElement.Parse((string)Provider.Generate(Script, new GraphOptions()));
+        }
+    }
+}
