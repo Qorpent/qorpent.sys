e30756dfa25c8dfa192539935a1f8391d30766b9
Myachin <i.myachin@ugmk.com>
2013-09-27 15:02:38 +0600

    chart render realization (issue AP-150) + chart test action

--- Qorpent.Mvc/Renders/FusionChartRender.cs
+++ Qorpent.Mvc/Renders/FusionChartRender.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using Qorpent.Dsl;
 
@@ -6,36 +7,45 @@ namespace Qorpent.Mvc.Renders {
     /// <summary>
     /// Render для отрисовки графиков Fusionchart
     /// </summary>
-    [Render("fc")]
+    [Render("chart")]
     public class FusionChartRender: RenderBase {
         /// <summary>
         /// Renders given context
         /// </summary>
         /// <param name="context"></param>
         public override void Render(IMvcContext context) {
-            var render = context.Get("render");
-            var type = ResolveChartType(context);
-            var id = context.Get("id", "fc-graph-" + DateTime.Now.Ticks);
+            var render = ResolveDataType(context);
+            var charttype = ResolveChartType(context, render);
+            var id = context.Get("__id", DateTime.Now.Ticks);
             var container = context.Get("__targetdiv");
-            var width = context.Get("width", "400");
-            var height = context.Get("height", "300");
-            var debug = context.Get("debug", "0");
+            var width = context.Get("__width", "400");
+            var height = context.Get("__height", "300");
+            var debug = context.Get("__debug", "0");
             var script = string.Empty;
+            var datascript = RenderDataScript(context, render);
+            var error = string.Empty;
 
-            if (!string.IsNullOrWhiteSpace(container)) {
-                script += string.Format(@"
-<div class=""fusinchart-cintainer"" id=""{0}""></div>", id);
-                container = id;
+            if (string.IsNullOrWhiteSpace(datascript)) {
+                error = "Нет данных для отображения";
             }
-            script += string.Format(@"
+
+            if (string.IsNullOrWhiteSpace(container)) {
+                container = "fc-graph-" + id;
+                script += string.Format(@"
+<div class=""fusinchart-container{0}"" id=""{1}"">{2}</div>", string.IsNullOrEmpty(error) ? " fusionchart-error" : "", container, error);
+            }
+
+            if (string.IsNullOrWhiteSpace(error)) {
+                script += string.Format(@"
 <script type=""text/javascript""><!--
-    {0}
-    var myChart = new FusionCharts('{1}', '{2}', '{3}', '{4}', '{5}');
-    {7}
+    FusionCharts.setCurrentRenderer('javascript');
+    var myChart = new FusionCharts('../charts/{0}.swf', '{1}', '{2}', '{3}', '{4}');
+    {5}
     myChart.render('{6}');
 // -->
-</script>", render == "javascript" ? "FusionCharts.setCurrentRenderer('"+ render + "');" : "", type, id, width, height, debug, container, RenderDataScript(context));
-            context.ContentType = "text/html";
+</script>", charttype, "fc-chart-" + id, width, height, debug, datascript, container);
+                context.ContentType = "text/html";   
+            }
 
             if (!string.IsNullOrWhiteSpace(context.Get("__standalone"))) {
                 script = @"
@@ -43,10 +53,11 @@ namespace Qorpent.Mvc.Renders {
 <header>
 </header>
 <body>
-<script type=""text/javascript"" src=""/scripts/jquery.min.js""></script>
-<script type=""text/javascript"" src=""/scripts/fusioncharts/FusionCharts.js""></script>
-<link rel=""stylesheet"" type=""text/css"" href=""/styles/fusioncharts/presentation.css""></link>
-" + script + @"
+<script type=""text/javascript"" src=""../scripts/jquery.min.js""></script>
+<script type=""text/javascript"" src=""../charts/FusionCharts.js""></script>
+<script type=""text/javascript"" src=""../charts/FusionCharts.HC.js""></script>
+<script type=""text/javascript"" src=""../charts/FusionCharts.HC.Charts.js""></script>
+<link rel=""stylesheet"" type=""text/css"" href=""../charts/presentation.css""></link>" + script + @"
 </body>
 </html>";
             }
@@ -54,14 +65,22 @@ namespace Qorpent.Mvc.Renders {
             context.Output.Write(script);
         }
 
-        private string ResolveChartType(IMvcContext context) {
+        private string ResolveChartType(IMvcContext context, string datatype = null) {
             var type = context.Get("__charttype");
             if (string.IsNullOrWhiteSpace(type)) {
-                if (context.ActionResult is XElement) {
-                    type = ((XElement)context.ActionResult).Attribute("charttype").Value;
+                if (null == datatype) datatype = ResolveDataType(context);
+                XAttribute typeattr = null;
+                if (datatype == "xml") {
+                    typeattr = ((XElement)context.ActionResult).Attribute("charttype");
                 }
-                if (context.ActionResult is String && context.ActionResult.ToString().IndexOf("{", StringComparison.InvariantCulture) != 0) {
-                    type = ResolveService<ISpecialXmlParser>("json.xml.parser").ParseXml((string)context.ActionResult).Attribute("charttype").Value;
+                if (datatype == "xmlstring") {
+                    typeattr = (XElement.Parse((string) context.ActionResult)).Attribute("charttype");
+                }
+                if (datatype == "json") {
+                    typeattr = ResolveService<ISpecialXmlParser>("json.xml.parser").ParseXml((string)context.ActionResult).Attribute("charttype");
+                }
+                if (null != typeattr) {
+                    type = typeattr.Value;
                 }
             }
             if (string.IsNullOrWhiteSpace(type)) {
@@ -70,29 +89,38 @@ namespace Qorpent.Mvc.Renders {
             return type;
         }
 
+        private string ResolveDataType(IMvcContext context) {
+            string type;
+            if (context.ActionResult is XElement) {
+                type = "xml";
+            } else if (context.ActionResult is String && context.ActionResult.ToString().Trim().StartsWith("<")) {
+                type = "xmlstring";
+            } else if (context.ActionResult is String && context.ActionResult.ToString().Trim().StartsWith("{")) {
+                type = "json";
+            } /*else if (context.ActionResult is IChartXmlSource) {
+                type = "xmlsource";
+            } else if (context.ActionResult is IChartSource) {
+                type = "source";
+            }*/ else {
+                type = "unknown";
+            }
+            return type;
+        }
+
         /// <summary>
         /// Render "set data" part of script 
         /// </summary>
         /// <param name="context"></param>
+        /// <param name="datatype"></param>
         /// <returns></returns>
-        private string RenderDataScript(IMvcContext context) {
-            var script = @"myChart.set{0}Data(""{1}"");";
-            string type;
-            var data = string.Empty;
-            if (context.ActionResult is XElement) {
-                type = "XML";
-                data = context.ActionResult.ToString();
+        private string RenderDataScript(IMvcContext context, string datatype = null) {
+            if (null == datatype) datatype = ResolveDataType(context);
+            if (null != context.ActionResult && (datatype == "xml" || datatype == "json" || datatype == "xmlstring")) {
+                var result = context.ActionResult.ToString();
+                result = Regex.Replace(result, @"\s{2,}", "");
+                return string.Format(@"myChart.set{0}Data(""{1}"");", datatype == "xmlstring" ? "XML" : datatype.ToUpper(), result);
             }
-            else if (context.ActionResult is String && context.ActionResult.ToString().StartsWith("{")) {
-                type = "JSON";
-                data = context.ActionResult.ToString();
-            }
-            else {
-                type = "JSON";
-                data = @"{""chart"": {""caption"" : ""Data is not supporting""},""data"" : []}";
-            }
-            script = string.Format(script, type, data);
-            return script;
+            return "";
         }
 
         /// <summary>
