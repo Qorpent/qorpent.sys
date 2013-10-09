b8de1b1e43f1a9966c3b9ea5ce6ba0a63b9a6198
Kirill Shirshov <remalloc@gmail.com>
2013-10-09 19:30:50 +0600

    Next and Previous Linq extensions added

---
+++ Qorpent.Utils/Extensions/LinqExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Qorpent.Utils.Extensions {
+    /// <summary>
+    ///     Расширения для Linq
+    /// </summary>
+    public static class LinqExtensions {
+        /// <summary>
+        ///     Позволяет получить следующий элемент текущей коллекции относительно текущего
+        /// </summary>
+        /// <typeparam name="T">Типизация коллекции</typeparam>
+        /// <param name="s">Исходная коллекция</param>
+        /// <param name="c">Текущий элемент</param>
+        /// <returns>Следующий элемент</returns>
+        public static T Next<T>(this IEnumerable<T> s, T c) {
+            return s.SkipWhile(_ => !_.Equals(c)).Skip(1).First();
+        }
+        /// <summary>
+        ///     Позволяет предыдущий следующий элемент текущей коллекции относительно текущего
+        /// </summary>
+        /// <typeparam name="T">Типизация коллекции</typeparam>
+        /// <param name="s">Исходная коллекция</param>
+        /// <param name="c">Текущий элемент</param>
+        /// <returns>Предыдущий элемент</returns>
+        public static T Previous<T>(this IEnumerable<T> s, T c) {
+            return s.TakeWhile(_ => !_.Equals(c)).Last();
+        }
+    }
+}
