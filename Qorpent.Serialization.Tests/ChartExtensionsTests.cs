4a921db75919fca8f74be27524cec4e71efa8b6f
Kirill Shirshov <remalloc@gmail.com>
2013-10-10 14:30:32 +0600

    AddElement extension added

---
+++ Qorpent.Serialization.Tests/ChartExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Qorpent.Charts;
+
+namespace Qorpent.Serialization.Tests {
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ChartExtensionsTests {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void CanAddElement() {
+            var chart = new Chart();
+            var ds = new ChartDataset();
+            var tt = new ChartSet();
+            tt.SetParentElement(ds);
+
+            chart.AddElement(ds).AddElement(tt);
+            Assert.AreEqual(1, chart.Datasets.Children.Count);
+            Assert.AreEqual(1, chart.Datasets.Children.First().Children.Count);
+        }
+    }
+}
---
+++ Qorpent.Serialization/Charts/ChartExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace Qorpent.Charts {
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ChartExtensions {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="chart"></param>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public static IChart AddElement(this IChart chart, IChartElement element) {
+            if (element is IChartDataset) {
+                chart.Datasets.Add(element as IChartDataset);
+            } else if (element is IChartCategory) {
+                chart.Categories.Add(element as IChartCategory);
+            } else if (element is IChartDataItem) {
+                var p = ParentOf<IChartDataset>(element);
+                
+                if (p != null) {
+                    p.Children.Add(element as IChartDataItem);
+                }
+            } else if (element is IChartTrendLine) {
+                chart.TrendLines.Add(element as IChartTrendLine);
+            } else if (element is IChartLineSet) {
+
+            }
+
+            return chart;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        private static T ParentOf<T>(IChartElement element) {
+            return element.Get<T>(ChartDefaults.ChartElementParentProperty);
+        }
+    }
+}
