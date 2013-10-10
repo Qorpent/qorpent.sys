146378433f133133d584a423167b65e97cbe4710
Kirill Shirshov <remalloc@gmail.com>
2013-10-10 17:02:55 +0100

    #Q-170 half-fixed

---
+++ Qorpent.IO.Tests/WebResourceProviderTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Qorpent.IO.Resources;
+using Qorpent.IO.Web;
+using Remalloc.Phoenix.CSWrapper;
+using System;
+
+namespace Qorpent.IO.Tests {
+    [TestFixture]
+    public class WebResourceProviderTests : PhoenixFixtureBase {
+        [Test]
+        public void GetSize() {
+            var content = "test";
+            LongSrv.Pages.Add("/test.html", content);
+            LongSrv.Synchronize();
+            var drp = new DefaultResourceProvider {
+                Extensions = new IResourceProviderExtension[] {new WebResourceProviderGetSizeExtension()}
+            };
+            Assert.AreEqual(content.Length, drp.GetSize(new Uri(LongSrv.GetUrl("/test.html"))));
+        }
+    }
+}
