30c83ddca6a3f940020d8afd96d4ada19420e12a
Kirill Shirshov <remalloc@gmail.com>
2013-10-17 16:01:38 +0600

    Canvas with scaling added

---
+++ Qorpent.Serialization/AbstractCanvas/CanvasPoint.cs
@@ -0,0 +1,37 @@
+﻿namespace Qorpent.AbstractCanvas {
+    /// <summary>
+    ///     Представление точки <see cref="ICanvas"/>
+    /// </summary>
+    public class CanvasPoint : ICanvasPoint {
+        /// <summary>
+        ///     Родительская канва
+        /// </summary>
+        public ICanvas Parent { get; private set; }
+        /// <summary>
+        ///     Позиция по оси X
+        /// </summary>
+        public double X { get; private set; }
+        /// <summary>
+        ///     Позиция по оси Y
+        /// </summary>
+        public double Y { get; private set; }
+        /// <summary>
+        ///     Представление точки <see cref="ICanvas"/>
+        /// </summary>
+        /// <param name="x">Позиция по X</param>
+        /// <param name="y">Позиция по Y</param>
+        public CanvasPoint(double x, double y) {
+            X = x;
+            Y = y;
+        }
+        /// <summary>
+        ///     Установка родительской канвы
+        /// </summary>
+        /// <param name="canvas">Родительский экземпляр <see cref="ICanvas"/></param>
+        /// <returns>Замыкание на <see cref="ICanvasPoint"/></returns>
+        public ICanvasPoint SetCanvas(ICanvas canvas) {
+            Parent = canvas;
+            return this;
+        }
+    }
+}
---
+++ Qorpent.Serialization/AbstractCanvas/ICanvasPrimitive.cs
@@ -0,0 +1,15 @@
+﻿namespace Qorpent.AbstractCanvas {
+    /// <summary>
+    ///     Представление примитива канвы
+    /// </summary>
+    public interface ICanvasPrimitive : ICanvasElement {
+        /// <summary>
+        ///     Позиция по оси X
+        /// </summary>
+        double X { get; }
+        /// <summary>
+        ///     Позиция по оси Y
+        /// </summary>
+        double Y { get; }
+    }
+}
