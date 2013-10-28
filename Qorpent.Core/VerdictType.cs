62caf8147425f3f3f249ee95a0fa399f1e9488f6
comdiv <fagim.sadykov@gmail.com>
2013-10-28 08:32:12 +0500

    verdict_and_trans_support

---
+++ Qorpent.Core/VerdictType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Qorpent {
+	/// <summary>
+	/// Тип вердикта
+	/// </summary>
+	[Flags]
+	public enum VerdictType {
+		/// <summary>
+		/// Решение на разрешение
+		/// </summary>
+		Allow = 1,
+		/// <summary>
+		/// Предупреждение
+		/// </summary>
+		Warning =2,
+		/// <summary>
+		/// Запрет
+		/// </summary>
+		Deny = 4,
+	}
+}
--- Qorpent.Core/Wiki/WikiPage.cs
+++ Qorpent.Core/Wiki/WikiPage.cs
@@ -9,6 +9,34 @@ namespace Qorpent.Wiki {
 	[Serialize]
 	public class WikiPage {
 		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="other"></param>
+		/// <returns></returns>
+		protected bool Equals(WikiPage other) {
+			return string.Equals(Code, other.Code) && Existed.Equals(other.Existed) && string.Equals(Text, other.Text) && LastWriteTime.Equals(other.LastWriteTime) && string.Equals(Owner, other.Owner) && string.Equals(Editor, other.Editor) && string.Equals(Title, other.Title) && string.Equals(Version, other.Version) && Published.Equals(other.Published) && string.Equals(Locker, other.Locker);
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		public override int GetHashCode() {
+			unchecked {
+				int hashCode = (Code != null ? Code.GetHashCode() : 0);
+				hashCode = (hashCode*397) ^ Existed.GetHashCode();
+				hashCode = (hashCode*397) ^ (Text != null ? Text.GetHashCode() : 0);
+				hashCode = (hashCode*397) ^ LastWriteTime.GetHashCode();
+				hashCode = (hashCode*397) ^ (Owner != null ? Owner.GetHashCode() : 0);
+				hashCode = (hashCode*397) ^ (Editor != null ? Editor.GetHashCode() : 0);
+				hashCode = (hashCode*397) ^ (Title != null ? Title.GetHashCode() : 0);
+				hashCode = (hashCode*397) ^ (Version != null ? Version.GetHashCode() : 0);
+				hashCode = (hashCode*397) ^ Published.GetHashCode();
+				hashCode = (hashCode*397) ^ (Locker != null ? Locker.GetHashCode() : 0);
+				return hashCode;
+			}
+		}
+
+		/// <summary>
 		/// Создает пустую страницу
 		/// </summary>
 		public WikiPage() {
@@ -71,5 +99,29 @@ namespace Qorpent.Wiki {
         ///     Юзер, залочивший страницу
         /// </summary>
         public string Locker { get; set; }
+		/// <summary>
+		/// Формирует копию страницы
+		/// </summary>
+		/// <returns></returns>
+		public WikiPage Clone() {
+			var result = MemberwiseClone() as WikiPage;
+			result.Propeties = new Dictionary<string, string>();
+			foreach (var propety in this.Propeties) {
+				result.Propeties[propety.Key] = propety.Value;
+			}
+			return result;
+		}
+		/// <summary>
+		/// Про
+		/// </summary>
+		/// <param name="obj"></param>
+		/// <returns></returns>
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+			return Equals((WikiPage) obj);
+		}
 	}
 }
