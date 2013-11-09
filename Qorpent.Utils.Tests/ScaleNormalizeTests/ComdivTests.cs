92e16ebd7ea4d3c5ce58c6faaf3deb37a7e38af0
comdiv <fagim.sadykov@gmail.com>
2013-11-09 17:08:49 +0500

    some_fixes

--- Qorpent.Utils.Tests/ScaleNormalizeTests/ComdivTests.cs
+++ Qorpent.Utils.Tests/ScaleNormalizeTests/ComdivTests.cs
@@ -3,8 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using Qorpent.Charts;
+using Qorpent.Utils.Extensions;
+using Qorpent.Utils.Scaling;
 
 namespace Qorpent.Utils.Tests.ScaleNormalizeTests {
-    public partial class ComdivRemallocTests {
+    public partial class WorkingScalesOnGMKPres {
+		[TestCase("543,520,528,516,494,490,523,555,547,526", 0, 600, 5, true, 600)]
+		public void UchalGokTests(string dataRow, double expectedMin, double expectedMax, double divline, bool checkDivlines, int height) {
+			ExecuteScaleTest(dataRow, expectedMin, expectedMax, divline, checkDivlines, height);
+		}
+
+	    private static void ExecuteScaleTest(string dataRow, double expectedMin, double expectedMax, double divline,
+	                                         bool checkDivlines, int height) {
+		    var data = dataRow.SmartSplit(false, true, new[] {','}).Select(Convert.ToDouble);
+		    var normalized = ScaleNormalizerImproved.Normalize(new ChartConfig {Height = height.ToString()}, data);
+		    Console.WriteLine("Expected: from {0} to {1} with {2} divlines", expectedMin, expectedMax, divline);
+		    Console.WriteLine("Gotten: from {0} to {1} with {2} divlines", normalized.Minimal, normalized.Maximal,
+		                      normalized.Divline);
+		    Assert.AreEqual(expectedMin, normalized.Minimal);
+		    Assert.AreEqual(expectedMax, normalized.Maximal);
+		    if (checkDivlines) {
+			    Assert.AreEqual(divline, normalized.Divline);
+		    }
+	    }
     }
 }
--- Qorpent.Utils.Tests/ScaleNormalizeTests/RemallocTests.cs
+++ Qorpent.Utils.Tests/ScaleNormalizeTests/RemallocTests.cs
@@ -6,18 +6,10 @@ using Qorpent.Utils.Extensions;
 using Qorpent.Utils.Scaling;
 
 namespace Qorpent.Utils.Tests.ScaleNormalizeTests {
-    public partial class ComdivRemallocTests {
+    public partial class WorkingScalesOnGMKPres {
         [TestCase("-744,999,-831,523,663,-1128,504,-591,325,-361", -1800.0, 1200.0, 4, true, 400)]
-        public void RemallocTestCases(string dataRow, double expectedMin, double expectedMax, double divline, bool checkDivlines, int height) {
-            var data = dataRow.SmartSplit(false, true, new[] { ',' }).Select(Convert.ToDouble);
-            var normalized = ScaleNormalizerImproved.Normalize(new ChartConfig {Height = height.ToString()}, data);
-            Console.WriteLine("Expected: from {0} to {1} with {2} divlines", expectedMin, expectedMax, divline);
-            Console.WriteLine("Gotten: from {0} to {1} with {2} divlines", normalized.Minimal, normalized.Maximal, normalized.Divline);
-            Assert.AreEqual(expectedMin, normalized.Minimal);
-            Assert.AreEqual(expectedMax, normalized.Maximal);
-            if (checkDivlines) {
-                Assert.AreEqual(divline, normalized.Divline);
-            }
+        public void GayGokTests(string dataRow, double expectedMin, double expectedMax, double divline, bool checkDivlines, int height) {
+			ExecuteScaleTest(dataRow, expectedMin, expectedMax, divline, checkDivlines, height);
         }
     }
 }
