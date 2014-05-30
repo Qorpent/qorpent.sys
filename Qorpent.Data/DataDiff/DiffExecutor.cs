a448fb373a1625220781209b2cd288b19108f8c4
comdiv <fagim.sadykov@gmail.com>
2014-05-30 09:49:00 +0600

    Log fixes (level set method added), diff - well log messages

--- Qorpent.Data/DataDiff/DiffExecutor.cs
+++ Qorpent.Data/DataDiff/DiffExecutor.cs
@@ -25,40 +25,40 @@ namespace Qorpent.Data.DataDiff
 		/// Выполняет весь цикл, включая обращение к БД
 		/// </summary>
 		public void Execute(){
-			_context.Log = ConsoleLogWriter.CreateLog("main",customFormat:"${Message}");
+			_context.Log = _context.Log?? ConsoleLogWriter.CreateLog("main",customFormat:"${Message}");
 			if (_context.DiffPairs == null){
-				_context.Log.Trace("require diff checking");
+				_context.Log.Info("require diff checking");
 				if (!string.IsNullOrWhiteSpace(_context.SqlConnectionString) && string.IsNullOrWhiteSpace(_context.GitBaseRevision)){
-					_context.Log.Trace("start revision determining");
+					_context.Log.Info("start revision determining");
 					new SqlSourceRevisionRetriever(_context).Determine();
-					_context.Log.Trace("base revision detected as " + _context.GitBaseRevision);
+					_context.Log.Info("base revision detected as " + _context.GitBaseRevision);
 				}
 				else{
-					_context.Log.Trace("sql server not set up - work for script gen only");
+					_context.Log.Info("sql server not set up - work for script gen only");
 				}
 				new DiffPairGenerator(_context).Generate();
-				_context.Log.Trace("difference pairs prepared evaluated");
+				_context.Log.Info("difference pairs prepared evaluated");
 			}
 			new DataTableDiffGenerator(_context).Generate();
-			_context.Log.Trace("difference tables generated: "+_context.Tables.Count()+" tables");
+			_context.Log.Info("difference tables generated: "+_context.Tables.Count()+" tables");
 			if (_context.Tables.Any()){
 				new SqlDiffGenerator(_context).Generate();
-				_context.Log.Trace("sql script prepared");
+				_context.Log.Info("sql script prepared");
 				if (_context.NoApply){
-					_context.Log.Trace("Применение изменений к БД отключено");
+					_context.Log.Info("Применение изменений к БД отключено");
 				}
 				else{
 					if (!string.IsNullOrWhiteSpace(_context.SqlConnectionString)){
 						new SqlUpdateApplyer(_context).Update();
-						_context.Log.Trace("sql database updated");
+						_context.Log.Info("sql database updated");
 					}
 					else{
-						_context.Log.Trace("sql not set up - no real apply to server");
+						_context.Log.Info("sql not set up - no real apply to server");
 					}
 				}
 			}
 			else{
-				_context.Log.Trace("Нет значимых изменений");
+				_context.Log.Info("Нет значимых изменений");
 			}
 		}
 	}
