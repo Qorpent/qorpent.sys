adaebebb72a7e1de4d9f5a808027e4e0e9a3c54b
Kirill Shirshov <remalloc@gmail.com>
2014-07-18 09:53:47 +0500

    #QPT-101

---
+++ Qorpent.Core/IntermediateFormat/IntermediateLayerFormat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Qorpent.IntermediateFormat {
+	/// <summary>
+	///		Описание слоёв документа промежуточного формата
+	/// </summary>
+	[Flags]
+	public enum IntermediateLayerFormat {
+		/// <summary>
+		///		Неопределённый слой
+		/// </summary>
+		Undefined = 0,
+		/// <summary>
+		///		Строка
+		/// </summary>
+		Row = 1,
+		/// <summary>
+		///		Колонка
+		/// </summary>
+		Col = 2,
+		/// <summary>
+		///		Особый слой (пересечение <see cref="Row"/> и <see cref="Col"/>)
+		/// </summary>
+		Special = 4,
+		/// <summary>
+		///		Слой данных
+		/// </summary>
+		Data,
+		/// <summary>
+		///		Слой настроек
+		/// </summary>
+		Settings,
+		/// <summary>
+		///		Слой информация
+		/// </summary>
+		Info,
+		/// <summary>
+		///		По умолчанию <see cref="Undefined"/>
+		/// </summary>
+		Default = Undefined
+	}
+}
