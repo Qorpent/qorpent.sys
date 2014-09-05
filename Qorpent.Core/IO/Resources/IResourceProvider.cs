b8575bb66cd395d7ec2a0c7bcb6d2b3439bc8aee
Kirill Shirshov <remalloc@gmail.com>
2014-09-05 12:55:57 +0600

    IResourceProvider comments normalized

--- Qorpent.Core/IO/Resources/IResourceProvider.cs
+++ Qorpent.Core/IO/Resources/IResourceProvider.cs
@@ -2,27 +2,26 @@
 
 namespace Qorpent.IO.Resources {
 	/// <summary>
-	/// Общее описание службы получения контента
+	///		Общее описание службы получения контента
 	/// </summary>
 	public interface IResourceProvider {
 		/// <summary>
-		/// Выполняет конфигурацию провайдера ресурсов
+		///		Выполняет конфигурацию провайдера ресурсов
 		/// </summary>
-		/// <param name="config"></param>
-		/// <returns></returns>
+		/// <param name="config">Конфигурация провайдера ресурсов</param>
 		void Configure(IResourceConfig config = null);
 		/// <summary>
-		/// Формирует объект запроса ресурса, который может использоваться для получения контента
+		///		Формирует объект запроса ресурса, который может использоваться для получения контента
 		/// </summary>
-		/// <param name="uri"></param>
-		/// <param name="config"></param>
-		/// <returns></returns>
+		/// <param name="uri">Uri документа</param>
+		/// <param name="config">Конфигурация провайдера ресурсов</param>
+		/// <returns>Запрос ресурса</returns>
 		IResourceRequest CreateRequest(Uri uri, IResourceConfig config = null);
 		/// <summary>
-		/// Проверка, что Uri может быть обработан
+		///		Проверка, что Uri может быть обработан
 		/// </summary>
-		/// <param name="uri"></param>
-		/// <returns></returns>
+		/// <param name="uri">Uri документа</param>
+		/// <returns>Признак возможности обработки ресурса</returns>
 		bool IsSupported(Uri uri);
         /// <summary>
         ///     Получение размера документа по его Uri
