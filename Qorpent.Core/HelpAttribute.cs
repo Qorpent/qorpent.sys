8e6401e552710a144384917c6e9a2437a395dfb8
Kirill Shirshov <remalloc@gmail.com>
2014-11-04 13:00:13 +0500

    Core: HelpAttribute added

---
+++ Qorpent.Core/HelpAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Qorpent {
+	/// <summary>
+	///		Атрибут описания сущности
+	/// </summary>
+	public class HelpAttribute : Attribute {
+		/// <summary>
+		///		Описание сущности
+		/// </summary>
+		public string Description { get; set; }
+		/// <summary>
+		///		Значение по умолчанию
+		/// </summary>
+		public string Default { get; set; }
+		/// <summary>
+		///		Атрибут описания сущности
+		/// </summary>
+		public HelpAttribute() { }
+		/// <summary>
+		///		Атрибут описания сущности
+		/// </summary>
+		/// <param name="description">Описание сущности</param>
+		public HelpAttribute(string description) {
+			Description = description;
+		}
+	}
+}
