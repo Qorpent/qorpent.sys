295dec060c73ca4e3816816d0995228c7a368c59
comdiv <fagim.sadykov@gmail.com>
2014-11-06 14:11:54 +0500

    Leaflet добавлен в состав базовых ресурсов SSL вынесен в zrepos

--- ssl/generatecerts.sh
+++
@@ -1,2 +0,0 @@
-openssl genrsa -out qorpentCA.key 2048
-openssl req -x509 -new -nodes -key qorpentCA.key -days 1024 -out qorpentCA.pem   -subj "/C=RU/ST=SV/L=EK/O=BIT/OU=IT_Department/CN=qorpent.myjetbrains.com"  -config "../../../bin/ssl/share/openssl.cnf"
