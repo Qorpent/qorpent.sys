dc57d7064049ec1927e7748f5f2f000aab7a4f73
comdiv <fagim.sadykov@gmail.com>
2015-04-01 19:32:18 +0500

    test fix

--- Qorpent.Scaffolding.Tests/SqlAndOrm/CacheWriterTest.cs
+++ Qorpent.Scaffolding.Tests/SqlAndOrm/CacheWriterTest.cs
@@ -38,7 +38,7 @@ class a prototype=dbtable
             var result = writer.ToString();
             Console.WriteLine(result);
             var simpleResult = Regex.Replace(result, @"\s", "").Replace("\"","'");
-            Assert.AreEqual(simpleResult, Regex.Replace(@"		private DbCommandWrapper _XWrapper = new DbCommandWrapper{ObjectName='X',Notation=DbCallNotation.Scalar};
+            Assert.AreEqual(simpleResult, Regex.Replace(@"		private DbCommandWrapper _XWrapper = new DbCommandWrapper{ObjectName='dbo.aX',Notation=DbCallNotation.Scalar};
 		///<summary>Bla-bla</summary>
 		public Int64 XGeneric (object parameters) {
 			var command = _XWrapper.Clone(parameters,GetConnection());
@@ -50,7 +50,7 @@ class a prototype=dbtable
 		public Int64 X (Int64 id = default(Int64)){
 			return XGeneric (new{id});
 		}
-		private DbCommandWrapper _YWrapper = new DbCommandWrapper{ObjectName='Y',Notation=DbCallNotation.MultipleReader};
+		private DbCommandWrapper _YWrapper = new DbCommandWrapper{ObjectName='dbo.aY',Notation=DbCallNotation.MultipleReader};
 		///<summary>Ta-da</summary>
 		public object YGeneric (object parameters) {
 			var command = _YWrapper.Clone(parameters,GetConnection());
