2104f5831efa50b9d935daf2cb674978dda0fb7f
safore <safore@gmail.com>
2015-04-07 19:16:24 +0600

    show sql command to trace if needed

--- Qorpent.Core/Utils/Extensions/DatabaseExtensions.cs
+++ Qorpent.Core/Utils/Extensions/DatabaseExtensions.cs
@@ -83,24 +83,29 @@ namespace Qorpent.Utils.Extensions
 		}
 
 
-		/// <summary>
-		/// Выполняет запрос без результатов
-		/// </summary>
-		/// <param name="connection"></param>
-		/// <param name="command"></param>
-		/// <param name="parameters"></param>
-		/// <param name="timeout"></param>
-		/// <param name="close"></param>
-		/// <exception cref="Exception"></exception>
-		public static int ExecuteNonQuery(this IDbConnection connection, object command,
-										   object parameters = null, int timeout=30, bool close = false)
+	    /// <summary>
+	    /// Выполняет запрос без результатов
+	    /// </summary>
+	    /// <param name="connection"></param>
+	    /// <param name="command"></param>
+	    /// <param name="parameters"></param>
+	    /// <param name="timeout"></param>
+	    /// <param name="close"></param>
+	    /// <param name="showCommandTextWithParams">показать в Debug</param>
+	    /// <exception cref="Exception"></exception>
+	    public static int ExecuteNonQuery(this IDbConnection connection, object command,
+										   object parameters = null, int timeout=30, bool close = false, bool showCommandTextWithParams = false)
 		{
 			connection.WellOpen();
 			IDbCommand cmd = connection.CreateCommand(command, parameters,timeout);
 			cmd.CommandTimeout = timeout;
 			int r;
 			try{
+                if (showCommandTextWithParams) {
+                    Trace.WriteLine("Sql: " + cmd.CommandAsSql(), "debug");
+			    }
 				r = cmd.ExecuteNonQuery();
+
 			}
 			catch (Exception ex){
 				throw new Exception("error in query:" + cmd.CommandText, ex);
@@ -114,6 +119,87 @@ namespace Qorpent.Utils.Extensions
 		}
 
 
+        public static String ParameterValueForSQL(this SqlParameter sp) {
+            String retval = "";
+
+            switch (sp.SqlDbType) {
+                case SqlDbType.Char:
+                case SqlDbType.NChar:
+                case SqlDbType.NText:
+                case SqlDbType.NVarChar:
+                case SqlDbType.Text:
+                case SqlDbType.Time:
+                case SqlDbType.VarChar:
+                case SqlDbType.Xml:
+                case SqlDbType.Date:
+                case SqlDbType.DateTime:
+                case SqlDbType.DateTime2:
+                case SqlDbType.DateTimeOffset:
+                    retval = "'" + sp.Value.ToString().Replace("'", "''") + "'";
+                    break;
+
+                case SqlDbType.Bit:
+                    retval = (sp.Value.ToString());
+                    break;
+
+                default:
+                    retval = sp.Value.ToString().Replace("'", "''");
+                    break;
+            }
+
+            return retval;
+        }
+
+        public static String CommandAsSql(this IDbCommand sc) {
+            StringBuilder sql = new StringBuilder();
+            Boolean FirstParam = true;
+
+            sql.AppendLine("use " + sc.Connection.Database + ";");
+            switch (sc.CommandType) {
+                case CommandType.StoredProcedure:
+                    sql.AppendLine("declare @return_value int;");
+
+                    foreach (SqlParameter sp in sc.Parameters) {
+                        if ((sp.Direction == ParameterDirection.InputOutput) || (sp.Direction == ParameterDirection.Output)) {
+                            sql.Append("declare " + sp.ParameterName + "\t" + sp.SqlDbType.ToString() + "\t= ");
+
+                            sql.AppendLine(((sp.Direction == ParameterDirection.Output) ? "null" : sp.ParameterValueForSQL()) + ";");
+
+                        }
+                    }
+
+                    sql.AppendLine("exec [" + sc.CommandText + "]");
+
+                    foreach (SqlParameter sp in sc.Parameters) {
+                        if (sp.Direction != ParameterDirection.ReturnValue) {
+                            sql.Append((FirstParam) ? "\t" : "\t, ");
+
+                            if (FirstParam) FirstParam = false;
+
+                            if (sp.Direction == ParameterDirection.Input)
+                                sql.AppendLine(sp.ParameterName + " = " + sp.ParameterValueForSQL());
+                            else
+
+                                sql.AppendLine(sp.ParameterName + " = " + sp.ParameterName + " output");
+                        }
+                    }
+                    sql.AppendLine(";");
+
+                    sql.AppendLine("select 'Return Value' = convert(varchar, @return_value);");
+
+                    foreach (SqlParameter sp in sc.Parameters) {
+                        if ((sp.Direction == ParameterDirection.InputOutput) || (sp.Direction == ParameterDirection.Output)) {
+                            sql.AppendLine("select '" + sp.ParameterName + "' = convert(varchar, " + sp.ParameterName + ");");
+                        }
+                    }
+                    break;
+                case CommandType.Text:
+                    sql.AppendLine(sc.CommandText);
+                    break;
+            }
+
+            return sql.ToString();
+        }
 		
 
 
