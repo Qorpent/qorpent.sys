15b5775d38c055d90f93fd919e015175a6f3c676
Kirill Shirshov <remalloc@gmail.com>
2015-04-16 19:21:07 +0500

    ITaskStateProvider interface added

---
+++ Qorpent.Core/Tasks/ITaskStateProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace Qorpent.Tasks {
+	/// <summary>
+	///		Провайдер состояний задач
+	/// </summary>
+	public interface ITaskStateProvider {
+		/// <summary>
+		///		Разрешает состояние задачи относительно её уникального идентификатора
+		/// </summary>
+		/// <param name="id">Идентификатор задачи</param>
+		/// <returns>Состояние задачи</returns>
+		TaskState GetState(long id);
+	}
+}
