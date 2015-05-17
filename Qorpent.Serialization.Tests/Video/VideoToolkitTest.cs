6a256053c250623633834ea7aff970be7fdf1653
comdiv <fagim.sadykov@gmail.com>
2015-05-18 03:01:31 +0500

    FFMPEG wrap #Q-477 Fixed

---
+++ Qorpent.Serialization.Tests/Video/VideoToolkitTest.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Qorpent.Utils;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Qorpent.Video;
+
+namespace Qorpent.Serialization.Tests.Video
+{
+    [TestFixture]
+    [Explicit]
+    public class VideoToolkitTest
+    {
+        private string dir;
+
+    
+
+        [SetUp]
+        public void Setup() {
+            dir = Path.Combine(Path.GetTempPath(), "VideoToolkitTest");
+            Directory.CreateDirectory(dir);
+        }
+
+        string GetPath(string file) {
+            return Path.Combine(dir, file);
+        }
+
+        [Test]
+        public void CanConvert() {
+            FileSystemHelper.DeleteIfExists(GetPath("slideshow-converted.mp4"));
+            FileSystemHelper.DeleteIfExists(GetPath("slideshow-converted.avi"));
+            MakeSlideShow();
+            var convertTask = new FFMpegConvertTask
+            {
+                SourceFile = GetPath("slideshow.mp4"),
+                TargetFile = GetPath("slideshow-converted.avi")
+            };
+            FFMpeg.Convert(convertTask);
+            convertTask = new FFMpegConvertTask {
+                SourceFile = GetPath("slideshow-converted.avi"),
+                TargetFile = GetPath("slideshow-converted.mp4")
+            };
+            FFMpeg.Convert(convertTask);
+            Assert.True(File.Exists(convertTask.TargetFile));
+            Assert.Greater(new FileInfo(convertTask.SourceFile).Length, new FileInfo(convertTask.TargetFile).Length);
+        }
+
+        [Test]
+        public void CanSplit() {
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "basis_01.mp4"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "basis_02.mp4"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "basis_03.mp4"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "basis_04.mp4"));
+            var slidetask = MakeSlideShow("long-slideshow.mp4",0.1m);
+            var splittask = new FFMpegVideoSplitTask {SourceFile = slidetask.TargetFile, WorkingFolder = dir};
+            splittask.Frames.Add(new FFMpegFrame{StartTime = TimeSpan.FromSeconds(1),Duration = TimeSpan.FromSeconds(1)});
+            splittask.Frames.Add(new FFMpegFrame{StartTime = TimeSpan.FromSeconds(11),Duration = TimeSpan.FromSeconds(2)});
+            splittask.Frames.Add(new FFMpegFrame{StartTime = TimeSpan.FromSeconds(1),Duration = TimeSpan.FromSeconds(0.5)});
+            splittask.Frames.Add(new FFMpegFrame{StartTime = TimeSpan.FromSeconds(12),Duration = TimeSpan.FromSeconds(2)});
+            FFMpeg.Split(splittask);
+            Assert.True(File.Exists(GetPath("basis_01.mp4")));
+            Assert.True(File.Exists(GetPath("basis_02.mp4")));
+            Assert.True(File.Exists(GetPath("basis_03.mp4")));
+            Assert.True(File.Exists(GetPath("basis_04.mp4")));
+        }
+
+        [Test]
+        public void CanSplitImages()
+        {
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "thumb_01.png"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "thumb_02.png"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "thumb_03.png"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "thumb_04.png"));
+            var slidetask = MakeSlideShow("long-slideshow.mp4", 0.1m);
+            var splittask = new FFMpegVideoSplitTask { SourceFile = slidetask.TargetFile, WorkingFolder = dir,FileNameBase = "thumb_"};
+            splittask.Frames.Add(new FFMpegFrame { StartTime = TimeSpan.FromSeconds(1), Duration = TimeSpan.FromSeconds(1) });
+            splittask.Frames.Add(new FFMpegFrame { StartTime = TimeSpan.FromSeconds(11), Duration = TimeSpan.FromSeconds(2) });
+            splittask.Frames.Add(new FFMpegFrame { StartTime = TimeSpan.FromSeconds(1), Duration = TimeSpan.FromSeconds(0.5) });
+            splittask.Frames.Add(new FFMpegFrame { StartTime = TimeSpan.FromSeconds(12), Duration = TimeSpan.FromSeconds(2) });
+            splittask.Width = 100;
+            splittask.Height = 100;
+            FFMpeg.SplitImages(splittask);
+            Assert.True(File.Exists(GetPath("thumb_01.png")));
+            Assert.True(File.Exists(GetPath("thumb_02.png")));
+            Assert.True(File.Exists(GetPath("thumb_03.png")));
+            Assert.True(File.Exists(GetPath("thumb_04.png")));
+        }
+
+        [Test]
+        public void CanJoin() {
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "joined.mp4"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "join-config.tmp"));
+            CanSplit();
+            var jointask = new FFMpegVideoJoinTask {
+                JoinFileName = GetPath("join-config.tmp"),
+                TargetFile = Path.Combine(dir, "joined.mp4"),
+                WorkingFolder = dir
+            };
+            FFMpeg.Join(jointask);
+            Assert.True(File.Exists(GetPath("joined.mp4")));
+            Assert.True(File.Exists(GetPath("join-config.tmp")));
+        }
+
+        [Test]
+        public void CanMakeMovie() {
+           
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "dirty-movie.avi"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "dirty-movie.mp4"));
+             FileSystemHelper.DeleteIfExists(Path.Combine(dir, "frame_01.mp4"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "frame_rawmovie.mp4"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "frame_02.mp4"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "frame_03.mp4"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "frame_04.mp4"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "movie.mp4"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "frame_movieset.tmp"));
+            // we cannot make avi directly but want to show ability to make movie from avi
+            MakeSlideShow("dirty-movie.mp4",0.1m);
+            FFMpeg.Convert(new FFMpegConvertTask{SourceFile = GetPath("dirty-movie.mp4"),TargetFile = GetPath("dirty-movie.avi"),});
+
+
+
+            var task = new FFMpegCreateMovieTask {
+                WorkingFolder = dir,
+                TmpFileBasis = "frame_",
+                SourceFile = GetPath("dirty-movie.avi"),
+                TargetFile = GetPath("movie.mp4"),
+
+            };
+            task.Frames.Add(new FFMpegFrame { StartTime = TimeSpan.FromSeconds(1), Duration = TimeSpan.FromSeconds(0.5) });
+            task.Frames.Add(new FFMpegFrame { StartTime = TimeSpan.FromSeconds(11), Duration = TimeSpan.FromSeconds(0.5) });
+            task.Frames.Add(new FFMpegFrame { StartTime = TimeSpan.FromSeconds(1), Duration = TimeSpan.FromSeconds(0.5) });
+            task.Frames.Add(new FFMpegFrame { StartTime = TimeSpan.FromSeconds(12), Duration = TimeSpan.FromSeconds(0.5) });
+            FFMpeg.CreateMovie(task);
+            Assert.True(File.Exists(GetPath("movie.mp4")));
+            Assert.True(File.Exists(GetPath("frame_movieset.tmp")));
+
+
+        }
+
+        [Test]
+        public void CanCreateSlideShow()
+        {
+           
+            var slidetask = MakeSlideShow();
+            Assert.True(File.Exists(slidetask.TargetFile));
+            Assert.Greater(8000, new FileInfo(slidetask.TargetFile).Length);
+
+        }
+       
+
+        private FFMpegSlideShowTask MakeSlideShow(string file = "slideshow.mp4", decimal slidesPerSecond= 0.5m) {
+            InitSlideFiles();
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, file));
+
+            var slidetask = new FFMpegSlideShowTask {
+                TargetFile = Path.Combine(dir, file),
+                WorkingFolder = dir,
+                SlidesPerSecond = slidesPerSecond
+            };
+            FFMpeg.CreateSlideShow(slidetask);
+            return slidetask;
+        }
+
+        private void InitSlideFiles() {
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "img_0001.png"));
+            FileSystemHelper.DeleteIfExists(Path.Combine(dir, "img_0002.png"));
+            var bmp = new Bitmap(40, 40);
+            using (Graphics gfx = Graphics.FromImage(bmp)) {
+                using (SolidBrush brush = new SolidBrush(Color.Red)) {
+                    gfx.FillRectangle(brush, 0, 0, 40, 40);
+                }
+                bmp.Save(Path.Combine(dir, "img_0001.png"), ImageFormat.Png);
+                using (SolidBrush brush = new SolidBrush(Color.Green)) {
+                    gfx.FillRectangle(brush, 0, 0, 40, 40);
+                }
+                bmp.Save(Path.Combine(dir, "img_0002.png"), ImageFormat.Png);
+            }
+        }
+    }
+}
---
+++ Qorpent.Serialization/Video/FFMpeg.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Qorpent.Utils;
+
+namespace Qorpent.Video
+{
+
+    public class FFMpegFrame {
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+
+    public class FFMpegVideoJoinTask {
+        public FFMpegVideoJoinTask() {
+            FileNameBase = "basis_";
+            DigitCount = 2;
+        }
+        public string WorkingFolder { get; set; }
+        public string FileNameBase { get; set; }
+        public int DigitCount { get; set; }
+        public string TargetFile { get; set; }
+        public string[] FileList { get; set; }
+        public string JoinFileName { get; set; }
+    }
+
+    public class FFMpegSlideShowTask
+    {
+        public FFMpegSlideShowTask()
+        {
+            FileNameBase = "img_";
+            DigitCount = 4;
+            SlidesPerSecond = 0.2m;
+            Height = 576;
+            Width = 720;
+            Fps = 25;
+        }
+        public string WorkingFolder { get; set; }
+        public int Fps { get; set; }
+        public string FileNameBase { get; set; }
+        public int DigitCount { get; set; }
+        public string TargetFile { get; set; }
+        public decimal SlidesPerSecond { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+
+
+    public class FFMpegVideoSplitTask
+    {
+        private IList<FFMpegFrame> _frames;
+
+        public FFMpegVideoSplitTask()
+        {
+            FileNameBase = "basis_";
+            DigitCount = 2;
+            Height = 576;
+            Width = 720;
+        }
+        public string WorkingFolder { get; set; }
+        public string FileNameBase { get; set; }
+        public int DigitCount { get; set; }
+        public string SourceFile { get; set; }
+        public IList<FFMpegFrame> Frames
+        {
+            get { return _frames ?? (_frames = new List<FFMpegFrame>()); }
+            set { _frames = value; }
+        }
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+
+    public class FFMpegConvertTask {
+        private IList<FFMpegFrame> _frames;
+
+        public FFMpegConvertTask()
+        {
+            StartTime = TimeSpan.FromSeconds(0);
+            Duration = TimeSpan.FromHours(5);
+            Height = 576;
+            Width = 720;
+        }
+        public string SourceFile { get; set; }
+        public string TargetFile { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+    }
+
+    public class FFMpegCreateMovieTask {
+        private IList<FFMpegFrame> _frames;
+
+        public FFMpegCreateMovieTask() {
+            StartTime = TimeSpan.FromSeconds(0);
+            Duration = TimeSpan.FromHours(5);
+            Height = 576;
+            Width = 720;
+            TmpFileBasis = "basis_";
+        }
+        public string Id { get; set; }
+        public string WorkingFolder { get; set; }
+        public string SourceFile { get; set; }
+        public string TargetFile { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string TmpFileBasis { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public IList<FFMpegFrame> Frames {
+            get { return _frames ??(_frames=new List<FFMpegFrame>()); }
+            set { _frames = value; }
+        }
+
+        public void Normalize() {
+            if (string.IsNullOrWhiteSpace(Id)) {
+                Id = "FFC" + DateTime.Now.ToString("YYYYMMDDHHmmss");
+            }
+            if (string.IsNullOrWhiteSpace(WorkingFolder)) {
+                WorkingFolder = Path.Combine(Path.GetTempPath(), Id);
+                
+            }
+            if (Duration.TotalMilliseconds < 300) {
+                Duration = TimeSpan.FromHours(5);
+            }
+            
+        }
+
+        public FFMpegCreateMovieTask Clone() {
+            var result = (FFMpegCreateMovieTask) MemberwiseClone();
+            result.Frames= new List<FFMpegFrame>();
+            foreach (var ffMpegFrame in this.Frames) {
+                result.Frames.Add(ffMpegFrame);
+            }
+            return result;
+
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class FFMpeg
+    {
+        
+
+        public static void CreateMovie(FFMpegCreateMovieTask task) {
+            task.Normalize();
+            Directory.CreateDirectory(task.WorkingFolder);
+            var convert = new FFMpegConvertTask();
+            convert.SourceFile = task.SourceFile;
+            convert.Height = task.Height;
+            convert.Width = task.Width;
+            convert.StartTime = task.StartTime;
+            convert.Duration = task.Duration;
+            convert.TargetFile = Path.Combine(task.WorkingFolder, task.TmpFileBasis+"rawmovie.mp4");
+            Convert(convert);
+            var splitter = new FFMpegVideoSplitTask();
+            splitter.SourceFile = convert.TargetFile;
+            splitter.Frames = task.Frames;
+            splitter.FileNameBase = task.TmpFileBasis;
+            splitter.WorkingFolder = task.WorkingFolder;
+            Split(splitter);
+            var joiner = new FFMpegVideoJoinTask();
+            joiner.TargetFile = task.TargetFile;
+            joiner.FileNameBase = task.TmpFileBasis;
+            joiner.JoinFileName = task.TmpFileBasis + "movieset.tmp";
+            joiner.WorkingFolder = task.WorkingFolder;
+            Join(joiner);
+        }
+
+        public static void CreateSlideShow(FFMpegSlideShowTask joiner) {
+            var handler = new ConsoleApplicationHandler();
+            handler.ExePath = EnvironmentInfo.GetExecutablePath("ffmpeg");
+            handler.WorkingDirectory = joiner.WorkingFolder;
+            var sb = new StringBuilder();
+            int rate = (int) joiner.SlidesPerSecond;
+            string ratestr = rate.ToString();
+            if (joiner.SlidesPerSecond < 1) {
+                rate = System.Convert.ToInt32(1/joiner.SlidesPerSecond);
+                ratestr = "1/" + rate;
+            }
+            sb.Append(" -framerate ");
+            sb.Append(ratestr);
+            sb.Append(" -i ");
+            sb.Append(joiner.FileNameBase);
+            sb.Append("%0");
+            sb.Append(joiner.DigitCount);
+            sb.Append("d.png");
+            sb.Append(" -c:v libx264 -y ");
+            var fps = joiner.Fps.ToString();
+            if (0 == joiner.Fps) fps = "25";
+            sb.Append(" -r ");
+            sb.Append(fps);
+            sb.Append(" -pix_fmt yuv420p ");
+            if (joiner.Height != 0 && joiner.Width != 0) {
+                sb.Append(" -s ");
+                sb.Append(joiner.Width);
+                sb.Append("x");
+                sb.Append(joiner.Height);
+            }
+            sb.Append(" \"");
+            sb.Append(joiner.TargetFile);
+            sb.Append("\"");
+            handler.Arguments = sb.ToString();
+            var result = handler.RunSync();
+            if (!result.IsOK) {
+                throw new Exception(result.Output+result.Error,result.Exception);
+            }
+        }
+
+        public static void Join(FFMpegVideoJoinTask joiner) {
+
+            var joinfile = CreateConcatFile(joiner);
+            var handler = new ConsoleApplicationHandler();
+            handler.ExePath = EnvironmentInfo.GetExecutablePath("ffmpeg");
+            var sb = new StringBuilder();
+            sb.Append(" -f concat");
+            sb.Append(" -i \"" + joinfile + "\" ");
+            sb.Append(" -c copy ");
+            sb.Append(" \"");
+            sb.Append(joiner.TargetFile);
+            sb.Append("\"");
+            handler.Arguments = sb.ToString();
+            var result = handler.RunSync();
+            if (!result.IsOK)
+            {
+                throw new Exception(result.Output+result.Error,result.Exception);
+            }
+        }
+
+        private static string CreateConcatFile(FFMpegVideoJoinTask joiner) {
+            var dir = joiner.WorkingFolder;
+            var tmpfile = joiner.JoinFileName;
+            if (string.IsNullOrWhiteSpace(tmpfile)) {
+                if (!string.IsNullOrWhiteSpace(dir)) {
+                    tmpfile = Path.Combine(dir, "join-config.tmp");
+                    if (File.Exists(tmpfile)) {
+                        tmpfile = Path.Combine(dir, "join-config-" + Environment.TickCount + ".tmp");
+                    }
+                }
+                else {
+                    tmpfile = Path.GetTempFileName();
+                }
+            }
+            else {
+                if (!Path.IsPathRooted(tmpfile)) {
+                    if (string.IsNullOrWhiteSpace(dir)) {
+                        dir = Environment.CurrentDirectory;
+                    }
+                    tmpfile = Path.Combine(dir, tmpfile);
+                }
+            }
+            if (null != joiner.FileList && 0 != joiner.FileList.Length) {
+                File.AppendAllLines(tmpfile, joiner.FileList.Select(_ => "file " + _.Replace("\\", "/")));
+            }
+            else {
+                if (string.IsNullOrWhiteSpace(dir)) {
+                    dir = Environment.CurrentDirectory;
+                }
+                var files = Directory.GetFiles(dir, joiner.FileNameBase + "*.mp4");
+                files =
+                    files.Where(
+                        _ =>
+                            Regex.IsMatch(Path.GetFileNameWithoutExtension(_),
+                                joiner.FileNameBase + "\\d{" + joiner.DigitCount + "}")).ToArray();
+                files = files.OrderBy(_ => Path.GetFileNameWithoutExtension(_)).ToArray();
+                File.AppendAllLines(tmpfile,files.Select(_=>"file "+_.Replace("\\","/")));
+            }
+            return tmpfile;
+        }
+
+        public static void Split(FFMpegVideoSplitTask splitter) {
+            if (string.IsNullOrWhiteSpace(splitter.WorkingFolder))
+            {
+                splitter.WorkingFolder = Path.GetDirectoryName(splitter.SourceFile);
+            }
+            Directory.CreateDirectory(splitter.WorkingFolder);
+            for (var i = 0; i < splitter.Frames.Count; i++) {
+                var frame = splitter.Frames[i];
+                var num = (i + 1).ToString("00");
+                var filename = splitter.FileNameBase + num + ".mp4";
+                var convert = new FFMpegConvertTask {
+                    SourceFile = splitter.SourceFile,
+                    TargetFile = Path.Combine(splitter.WorkingFolder, filename),
+                    StartTime = frame.StartTime,
+                    Duration = frame.Duration,
+                    Width = splitter.Width,
+                    Height = splitter.Height
+                };
+                Convert(convert);
+            }
+        }
+
+
+        public static void SplitImages(FFMpegVideoSplitTask splitter)
+        {
+            if (string.IsNullOrWhiteSpace(splitter.WorkingFolder)) {
+                splitter.WorkingFolder = Path.GetDirectoryName(splitter.SourceFile);
+            }
+            Directory.CreateDirectory(splitter.WorkingFolder);
+            for (var i = 0; i < splitter.Frames.Count; i++)
+            {
+                var frame = splitter.Frames[i];
+                var num = (i + 1).ToString("00");
+                var filename = splitter.FileNameBase + num + ".png";
+                if (splitter.DigitCount == 0 && splitter.Frames.Count==1) {
+
+                    filename = splitter.FileNameBase+ ".png";
+                }
+                filename = Path.Combine(splitter.WorkingFolder, filename);
+                var handler = new ConsoleApplicationHandler();
+                handler.ExePath = EnvironmentInfo.GetExecutablePath("ffmpeg");
+                var sb = new StringBuilder();
+                sb.Append(" -ss ");
+                sb.Append(frame.StartTime);
+                sb.Append(" -i ");
+                sb.Append("\"");
+                sb.Append(splitter.SourceFile);
+                sb.Append("\" ");
+                sb.Append(" -vframes 1 ");
+                if (splitter.Height != 0 && splitter.Width != 0)
+                {
+                    sb.Append(" -s ");
+                    sb.Append(splitter.Width);
+                    sb.Append("x");
+                    sb.Append(splitter.Height);
+                }
+                sb.Append(" -y -f image2 ");
+                sb.Append("\"");
+                sb.Append(filename);
+                sb.Append("\"");
+                handler.Arguments = sb.ToString();
+                var result = handler.RunSync();
+                if (!result.IsOK)
+                {
+                    throw new Exception(result.Output+result.Error, result.Exception);
+                }
+            }
+        }
+
+        public static void Convert(FFMpegConvertTask task)
+        {
+            var handler = new ConsoleApplicationHandler();
+            handler.ExePath = EnvironmentInfo.GetExecutablePath("ffmpeg");
+
+            var sb = new StringBuilder();
+            sb.Append(" -i ");
+            sb.Append(task.SourceFile);
+            sb.Append(" -acodec libfaac -b:a 128k -vcodec libx264 -b:v 1200k -y ");
+            if (task.Height != 0 && task.Width != 0)
+            {
+                sb.Append(" -s ");
+                sb.Append(task.Width);
+                sb.Append("x");
+                sb.Append(task.Height);
+            }
+            sb.Append(" -ss ");
+            sb.Append(task.StartTime);
+            sb.Append(" -t ");
+            if (task.Duration.TotalMilliseconds < 100) {
+                sb.Append("05:00:00");
+            }
+            else {
+                sb.Append(task.Duration);
+            }
+            sb.Append(" -flags +aic+mv4 ");
+            sb.Append(" \"");
+            sb.Append(task.TargetFile);
+            sb.Append("\"");
+            handler.Arguments = sb.ToString();
+            var result = handler.RunSync();
+            if (!result.IsOK)
+            {
+                throw new Exception(result.Output+result.Error, result.Exception);
+            }
+        }
+
+        
+    }
+}
