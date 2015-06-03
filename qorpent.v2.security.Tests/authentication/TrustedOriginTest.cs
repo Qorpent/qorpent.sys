87fec71c4bd6f34d9a71146dd25efd39a51dfb0f
comdiv <fagim.sadykov@gmail.com>
2015-06-03 19:26:19 +0500

    loggable trusted origin

---
+++ qorpent.v2.security.Tests/authentication/TrustedOriginTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using qorpent.v2.security.authentication;
+
+namespace qorpent.v2.security.Tests.authentication
+{
+    [TestFixture]
+    public class TrustedOriginTest
+    {
+        [Test]
+        public void RipToLip() {
+            var to = new TrustedOrigin();
+            to.RemoteAddressName = "111.111.111.111";
+            to.LocalAddressName = "112.112.112.112";
+            Assert.True(to.IsMatch(
+                new IPEndPoint(IPAddress.Parse("111.111.111.111"),123), 
+                 new IPEndPoint(IPAddress.Parse("112.112.112.112"),123),
+                 "http://any.com"
+                ));
+            Assert.False(to.IsMatch(
+                new IPEndPoint(IPAddress.Parse("111.111.111.112"), 123),
+                 new IPEndPoint(IPAddress.Parse("112.112.112.112"), 123),
+                 "http://any.com"
+                ));
+            Assert.False(to.IsMatch(
+               new IPEndPoint(IPAddress.Parse("111.111.111.111"), 123),
+                new IPEndPoint(IPAddress.Parse("112.112.112.113"), 123),
+                "http://any.com"
+               ));
+        }
+    }
+}
--- qorpent.v2.security/authentication/TrustedOrigin.cs
+++ qorpent.v2.security/authentication/TrustedOrigin.cs
@@ -1,16 +1,72 @@
 using System.Net;
+using Qorpent.Experiments;
+using Qorpent.Log.NewLog;
 
 namespace qorpent.v2.security.authentication {
     public class TrustedOrigin {
+        private ILoggy logg;
+        private IPAddress _localAddress;
+        private IPAddress _remoteAddress;
+
+        public TrustedOrigin() {
+            this.logg = Loggy.Get("sys.sec.trusted");
+
+        }
         public string LocalAddressName { get; set; }
         public string RemoteAddressName { get; set; }
-        public IPAddress LocalAddress { get; set; }
-        public IPAddress RemoteAddress { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IPAddress LocalAddress {
+            get {
+                if (null == _localAddress && !string.IsNullOrWhiteSpace(LocalAddressName) && LocalAddressName!="*") {
+                    _localAddress = IPAddress.Parse(LocalAddressName);
+                }
+                return _localAddress;
+            }
+            set { _localAddress = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IPAddress RemoteAddress {
+            get {
+                if (null == _remoteAddress && !string.IsNullOrWhiteSpace(RemoteAddressName) && RemoteAddressName != "*")
+                {
+                    _remoteAddress = IPAddress.Parse(RemoteAddressName);
+                }
+                return _remoteAddress;
+            }
+            set { _remoteAddress = value; }
+        }
+
         public int LocalPort { get; set; }
         public string Origin { get; set; }
         public string Name { get; set; }
 
         public bool IsMatch(IPEndPoint remote, IPEndPoint local, string origin) {
+            
+            var result =  InternalMatch(remote, local, origin);
+            if (logg.IsForDebug()) {
+                var loggm = new {
+                    lan = LocalAddressName,
+                    ran = RemoteAddressName,
+                    lp = LocalPort,
+                    o = Origin,
+                    n = Name,
+                    rm = remote.ToString(),
+                    lc = local.ToString(),
+                    or = origin,
+                    rs = result
+                };
+                logg.Debug(loggm.stringify());
+            }
+            return result;
+        }
+
+        private bool InternalMatch(IPEndPoint remote, IPEndPoint local, string origin) {
             if (!string.IsNullOrWhiteSpace(Origin) && Origin != "*") {
                 if (Origin != origin) {
                     return false;
