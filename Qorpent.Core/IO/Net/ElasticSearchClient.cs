c6f192b1dda02989a93482f15d0d509cdb0f4d60
comdiv <fagim.sadykov@gmail.com>
2015-06-04 02:37:44 +0500

    x10 es timeout

--- Qorpent.Core/IO/Net/ElasticSearchClient.cs
+++ Qorpent.Core/IO/Net/ElasticSearchClient.cs
@@ -6,7 +6,7 @@ using Qorpent.IO.Net;
 namespace qorpent.v2.security.utils {
     public class ElasticSearchClient {
         public ElasticSearchClient() {
-            ConnectionTimeout = 200;
+            ConnectionTimeout = 2000;
             Urls = new List<string> {"http://127.0.0.1:9200"};
         }
 
