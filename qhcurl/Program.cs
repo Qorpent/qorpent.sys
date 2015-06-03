25bbbc3da6eb5ae49adb340d15a11d485e66ed5d
comdiv <fagim.sadykov@gmail.com>
2015-06-04 01:16:51 +0500

    qhcurl added

---
+++ qhcurl/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Qorpent.IO.Net;
+
+namespace qhcurl
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var cli = new HttpClient();
+            Console.WriteLine(cli.GetString(args[0]));
+        }
+    }
+}
