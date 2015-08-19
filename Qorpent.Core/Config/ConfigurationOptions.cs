a0c7c5281f1a54a63ae9e90a9d044cd274ee75d0
comdiv <fagim.sadykov@gmail.com>
2015-08-19 17:09:36 +0500

    4.5 compatibility

--- Qorpent.Core/Config/ConfigurationOptions.cs
+++ Qorpent.Core/Config/ConfigurationOptions.cs
@@ -2,9 +2,13 @@ using Qorpent.IO;
 
 namespace Qorpent.Config {
     public class ConfigurationOptions {
+        public ConfigurationOptions() {
+            ConfigNameBSharpPrefix = "APP";
+            AddNameToGlobalConditions = true;
+        }
         public string Name { get; set; }
         public FileSet FileSet { get; set; }
-        public string ConfigNameBSharpPrefix { get; set; } = "APP";
-        public bool AddNameToGlobalConditions { get; set; } = true;
+        public string ConfigNameBSharpPrefix { get; set; }
+        public bool AddNameToGlobalConditions { get; set; }
     }
 }
